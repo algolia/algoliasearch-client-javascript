8ec0d606a944cd8a66e282044bf55a5fc741444b
Sylvain UTARD <sylvain.utard@gmail.com>
2013-11-08 19:17:24 +0100

    Encode array-based search params.

--- src/algoliasearch.js
+++ src/algoliasearch.js
@@ -420,7 +420,7 @@ AlgoliaSearch.prototype = {
         for (var key in args) {
             if (key != null && args.hasOwnProperty(key)) {
                 params += (params.length === 0) ? '?' : '&';
-                params += key + '=' + encodeURIComponent(args[key]);
+                params += key + '=' + encodeURIComponent(Object.prototype.toString.call(args[key]) === '[object Array]' ? JSON.stringify(args[key]) : args[key]);
             }
         }
         return params;
