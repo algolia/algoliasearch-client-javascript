adfa4386ea6a8dcffd2ef30bc3bf8cdbd3b2e854
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-03 15:26:09 +0100

    Fixed coding style (grunt lint)

--- src/algoliasearch.js
+++ src/algoliasearch.js
@@ -221,8 +221,8 @@ AlgoliaSearch.prototype = {
      *   - settings : allows to get index settings (https only)
      *   - editSettings : allows to change index settings (https only)
      * @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-     * @param maxQueriesPerIPPerHour Specify the maximum number of API calls allowed from an IP address per hour. 
-     * @param maxHitsPerQuery Specify the maximum number of hits this API key can retrieve in one call. 
+     * @param maxQueriesPerIPPerHour Specify the maximum number of API calls allowed from an IP address per hour.
+     * @param maxHitsPerQuery Specify the maximum number of hits this API key can retrieve in one call.
      * @param callback the result callback with two arguments
      *  success: boolean set to true if the request was successfull
      *  content: the server answer with user keys list or error description if success is false.
@@ -635,57 +635,57 @@ AlgoliaSearch.prototype.Index.prototype = {
          * - hitsPerPage: (integer) Pagination parameter used to select the number of hits per page. Defaults to 20.
          * - attributesToRetrieve: a string that contains the list of object attributes you want to retrieve (let you minimize the answer size).
          *   Attributes are separated with a comma (for example "name,address").
-         *   You can also use a string array encoding (for example ["name","address"]). 
+         *   You can also use a string array encoding (for example ["name","address"]).
          *   By default, all attributes are retrieved. You can also use '*' to retrieve all values when an attributesToRetrieve setting is specified for your index.
-         * - attributesToHighlight: a string that contains the list of attributes you want to highlight according to the query. 
-         *   Attributes are separated by a comma. You can also use a string array encoding (for example ["name","address"]). 
-         *   If an attribute has no match for the query, the raw value is returned. By default all indexed text attributes are highlighted. 
-         *   You can use `*` if you want to highlight all textual attributes. Numerical attributes are not highlighted. 
+         * - attributesToHighlight: a string that contains the list of attributes you want to highlight according to the query.
+         *   Attributes are separated by a comma. You can also use a string array encoding (for example ["name","address"]).
+         *   If an attribute has no match for the query, the raw value is returned. By default all indexed text attributes are highlighted.
+         *   You can use `*` if you want to highlight all textual attributes. Numerical attributes are not highlighted.
          *   A matchLevel is returned for each highlighted attribute and can contain:
          *      - full: if all the query terms were found in the attribute,
          *      - partial: if only some of the query terms were found,
          *      - none: if none of the query terms were found.
-         * - attributesToSnippet: a string that contains the list of attributes to snippet alongside the number of words to return (syntax is `attributeName:nbWords`). 
+         * - attributesToSnippet: a string that contains the list of attributes to snippet alongside the number of words to return (syntax is `attributeName:nbWords`).
          *    Attributes are separated by a comma (Example: attributesToSnippet=name:10,content:10).
          *    You can also use a string array encoding (Example: attributesToSnippet: ["name:10","content:10"]). By default no snippet is computed.
          * - minWordSizefor1Typo: the minimum number of characters in a query word to accept one typo in this word. Defaults to 3.
          * - minWordSizefor2Typos: the minimum number of characters in a query word to accept two typos in this word. Defaults to 7.
          * - getRankingInfo: if set to 1, the result hits will contain ranking information in _rankingInfo attribute.
          * - aroundLatLng: search for entries around a given latitude/longitude (specified as two floats separated by a comma).
-         *   For example aroundLatLng=47.316669,5.016670). 
+         *   For example aroundLatLng=47.316669,5.016670).
          *   You can specify the maximum distance in meters with the aroundRadius parameter (in meters) and the precision for ranking with aroundPrecision
          *   (for example if you set aroundPrecision=100, two objects that are distant of less than 100m will be considered as identical for "geo" ranking parameter).
          *   At indexing, you should specify geoloc of an object with the _geoloc attribute (in the form {"_geoloc":{"lat":48.853409, "lng":2.348800}})
          * - insideBoundingBox: search entries inside a given area defined by the two extreme points of a rectangle (defined by 4 floats: p1Lat,p1Lng,p2Lat,p2Lng).
          *   For example insideBoundingBox=47.3165,4.9665,47.3424,5.0201).
          *   At indexing, you should specify geoloc of an object with the _geoloc attribute (in the form {"_geoloc":{"lat":48.853409, "lng":2.348800}})
-         * - numericFilters: a string that contains the list of numeric filters you want to apply separated by a comma. 
-         *   The syntax of one filter is `attributeName` followed by `operand` followed by `value`. Supported operands are `<`, `<=`, `=`, `>` and `>=`. 
-         *   You can have multiple conditions on one attribute like for example numericFilters=price>100,price<1000. 
+         * - numericFilters: a string that contains the list of numeric filters you want to apply separated by a comma.
+         *   The syntax of one filter is `attributeName` followed by `operand` followed by `value`. Supported operands are `<`, `<=`, `=`, `>` and `>=`.
+         *   You can have multiple conditions on one attribute like for example numericFilters=price>100,price<1000.
          *   You can also use a string array encoding (for example numericFilters: ["price>100","price<1000"]).
-         * - tagFilters: filter the query by a set of tags. You can AND tags by separating them by commas. 
+         * - tagFilters: filter the query by a set of tags. You can AND tags by separating them by commas.
          *   To OR tags, you must add parentheses. For example, tags=tag1,(tag2,tag3) means tag1 AND (tag2 OR tag3).
          *   You can also use a string array encoding, for example tagFilters: ["tag1",["tag2","tag3"]] means tag1 AND (tag2 OR tag3).
-         *   At indexing, tags should be added in the _tags** attribute of objects (for example {"_tags":["tag1","tag2"]}). 
-         * - facetFilters: filter the query by a list of facets. 
-         *   Facets are separated by commas and each facet is encoded as `attributeName:value`. 
-         *   For example: `facetFilters=category:Book,author:John%20Doe`. 
+         *   At indexing, tags should be added in the _tags** attribute of objects (for example {"_tags":["tag1","tag2"]}).
+         * - facetFilters: filter the query by a list of facets.
+         *   Facets are separated by commas and each facet is encoded as `attributeName:value`.
+         *   For example: `facetFilters=category:Book,author:John%20Doe`.
          *   You can also use a string array encoding (for example `["category:Book","author:John%20Doe"]`).
-         * - facets: List of object attributes that you want to use for faceting. 
-         *   Attributes are separated with a comma (for example `"category,author"` ). 
+         * - facets: List of object attributes that you want to use for faceting.
+         *   Attributes are separated with a comma (for example `"category,author"` ).
          *   You can also use a JSON string array encoding (for example ["category","author"]).
-         *   Only attributes that have been added in **attributesForFaceting** index setting can be used in this parameter. 
+         *   Only attributes that have been added in **attributesForFaceting** index setting can be used in this parameter.
          *   You can also use `*` to perform faceting on all attributes specified in **attributesForFaceting**.
          * - queryType: select how the query words are interpreted, it can be one of the following value:
          *    - prefixAll: all query words are interpreted as prefixes,
          *    - prefixLast: only the last word is interpreted as a prefix (default behavior),
          *    - prefixNone: no query word is interpreted as a prefix. This option is not recommended.
-         * - optionalWords: a string that contains the list of words that should be considered as optional when found in the query. 
+         * - optionalWords: a string that contains the list of words that should be considered as optional when found in the query.
          *   The list of words is comma separated.
-         * - distinct: If set to 1, enable the distinct feature (disabled by default) if the attributeForDistinct index setting is set. 
-         *   This feature is similar to the SQL "distinct" keyword: when enabled in a query with the distinct=1 parameter, 
-         *   all hits containing a duplicate value for the attributeForDistinct attribute are removed from results. 
-         *   For example, if the chosen attribute is show_name and several hits have the same value for show_name, then only the best 
+         * - distinct: If set to 1, enable the distinct feature (disabled by default) if the attributeForDistinct index setting is set.
+         *   This feature is similar to the SQL "distinct" keyword: when enabled in a query with the distinct=1 parameter,
+         *   all hits containing a duplicate value for the attributeForDistinct attribute are removed from results.
+         *   For example, if the chosen attribute is show_name and several hits have the same value for show_name, then only the best
          *   one is kept and others are removed.
          * @param delay (optional) if set, wait for this delay (in ms) and only send the query if there was no other in the meantime.
          */
@@ -715,9 +715,9 @@ AlgoliaSearch.prototype.Index.prototype = {
          */
         browse: function(page, callback, hitsPerPage) {
             var indexObj = this;
-            var params = "?page=" + page;
+            var params = '?page=' + page;
             if (!_.isUndefined(hitsPerPage)) {
-                params += "&hitsPerPage=" + hitsPerPage;
+                params += '&hitsPerPage=' + hitsPerPage;
             }
             this.as._jsonRequest({ method: 'GET',
                                    url: '/1/indexes/' + encodeURIComponent(indexObj.indexName) + '/browse' + params,
@@ -727,7 +727,7 @@ AlgoliaSearch.prototype.Index.prototype = {
         /*
          * Get transport layer for Typeahead.js
          * @param args (optional) if set, contains an object with query parameters (see search for details)
-         * @param propertyName(optional) if set, contains the name of property that will be used for 
+         * @param propertyName(optional) if set, contains the name of property that will be used for
          */
         getTypeaheadTransport: function(args, valueOption) {
             this.typeAheadArgs = args;
@@ -748,7 +748,7 @@ AlgoliaSearch.prototype.Index.prototype = {
 
                         if (typeof obj.value === 'undefined') {
                             if (self.typeAheadValueOption != null) {
-                                if (typeof self.typeAheadValueOption === "function") {
+                                if (typeof self.typeAheadValueOption === 'function') {
                                     obj.value = self.typeAheadValueOption(obj);
                                     found = true;
                                 } else if (typeof obj[self.typeAheadValueOption] !== 'undefined') {
@@ -830,42 +830,42 @@ AlgoliaSearch.prototype.Index.prototype = {
          * - minWordSizefor1Typo: (integer) the minimum number of characters to accept one typo (default = 3).
          * - minWordSizefor2Typos: (integer) the minimum number of characters to accept two typos (default = 7).
          * - hitsPerPage: (integer) the number of hits per page (default = 10).
-         * - attributesToRetrieve: (array of strings) default list of attributes to retrieve in objects. 
+         * - attributesToRetrieve: (array of strings) default list of attributes to retrieve in objects.
          *   If set to null, all attributes are retrieved.
-         * - attributesToHighlight: (array of strings) default list of attributes to highlight. 
+         * - attributesToHighlight: (array of strings) default list of attributes to highlight.
          *   If set to null, all indexed attributes are highlighted.
          * - attributesToSnippet**: (array of strings) default list of attributes to snippet alongside the number of words to return (syntax is attributeName:nbWords).
          *   By default no snippet is computed. If set to null, no snippet is computed.
          * - attributesToIndex: (array of strings) the list of fields you want to index.
          *   If set to null, all textual and numerical attributes of your objects are indexed, but you should update it to get optimal results.
          *   This parameter has two important uses:
-         *     - Limit the attributes to index: For example if you store a binary image in base64, you want to store it and be able to 
+         *     - Limit the attributes to index: For example if you store a binary image in base64, you want to store it and be able to
          *       retrieve it but you don't want to search in the base64 string.
-         *     - Control part of the ranking*: (see the ranking parameter for full explanation) Matches in attributes at the beginning of 
-         *       the list will be considered more important than matches in attributes further down the list. 
-         *       In one attribute, matching text at the beginning of the attribute will be considered more important than text after, you can disable 
+         *     - Control part of the ranking*: (see the ranking parameter for full explanation) Matches in attributes at the beginning of
+         *       the list will be considered more important than matches in attributes further down the list.
+         *       In one attribute, matching text at the beginning of the attribute will be considered more important than text after, you can disable
          *       this behavior if you add your attribute inside `unordered(AttributeName)`, for example attributesToIndex: ["title", "unordered(text)"].
-         * - attributesForFaceting: (array of strings) The list of fields you want to use for faceting. 
+         * - attributesForFaceting: (array of strings) The list of fields you want to use for faceting.
          *   All strings in the attribute selected for faceting are extracted and added as a facet. If set to null, no attribute is used for faceting.
-         * - attributeForDistinct: (string) The attribute name used for the Distinct feature. This feature is similar to the SQL "distinct" keyword: when enabled 
-         *   in query with the distinct=1 parameter, all hits containing a duplicate value for this attribute are removed from results. 
+         * - attributeForDistinct: (string) The attribute name used for the Distinct feature. This feature is similar to the SQL "distinct" keyword: when enabled
+         *   in query with the distinct=1 parameter, all hits containing a duplicate value for this attribute are removed from results.
          *   For example, if the chosen attribute is show_name and several hits have the same value for show_name, then only the best one is kept and others are removed.
          * - ranking: (array of strings) controls the way results are sorted.
-         *   We have six available criteria: 
+         *   We have six available criteria:
          *    - typo: sort according to number of typos,
          *    - geo: sort according to decreassing distance when performing a geo-location based search,
          *    - proximity: sort according to the proximity of query words in hits,
          *    - attribute: sort according to the order of attributes defined by attributesToIndex,
-         *    - exact: 
-         *        - if the user query contains one word: sort objects having an attribute that is exactly the query word before others. 
-         *          For example if you search for the "V" TV show, you want to find it with the "V" query and avoid to have all popular TV 
+         *    - exact:
+         *        - if the user query contains one word: sort objects having an attribute that is exactly the query word before others.
+         *          For example if you search for the "V" TV show, you want to find it with the "V" query and avoid to have all popular TV
          *          show starting by the v letter before it.
          *        - if the user query contains multiple words: sort according to the number of words that matched exactly (and not as a prefix).
          *    - custom: sort according to a user defined formula set in **customRanking** attribute.
          *   The standard order is ["typo", "geo", "proximity", "attribute", "exact", "custom"]
          * - customRanking: (array of strings) lets you specify part of the ranking.
          *   The syntax of this condition is an array of strings containing attributes prefixed by asc (ascending order) or desc (descending order) operator.
-         *   For example `"customRanking" => ["desc(population)", "asc(name)"]`  
+         *   For example `"customRanking" => ["desc(population)", "asc(name)"]`
          * - queryType: Select how the query words are interpreted, it can be one of the following value:
          *   - prefixAll: all query words are interpreted as prefixes,
          *   - prefixLast: only the last word is interpreted as a prefix (default behavior),
@@ -959,8 +959,8 @@ AlgoliaSearch.prototype.Index.prototype = {
          *   - settings : allows to get index settings (https only)
          *   - editSettings : allows to change index settings (https only)
          * @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-         * @param maxQueriesPerIPPerHour Specify the maximum number of API calls allowed from an IP address per hour. 
-         * @param maxHitsPerQuery Specify the maximum number of hits this API key can retrieve in one call. 
+         * @param maxQueriesPerIPPerHour Specify the maximum number of API calls allowed from an IP address per hour.
+         * @param maxHitsPerQuery Specify the maximum number of hits this API key can retrieve in one call.
          * @param callback the result callback with two arguments
          *  success: boolean set to true if the request was successfull
          *  content: the server answer with user keys list or error description if success is false.
@@ -969,7 +969,7 @@ AlgoliaSearch.prototype.Index.prototype = {
             var indexObj = this;
             var aclsObject = {};
             aclsObject.acl = acls;
-            aclsObject.validity = validity;            
+            aclsObject.validity = validity;
             aclsObject.maxQueriesPerIPPerHour = maxQueriesPerIPPerHour;
             aclsObject.maxHitsPerQuery = maxHitsPerQuery;
             this.as._jsonRequest({ method: 'POST',
