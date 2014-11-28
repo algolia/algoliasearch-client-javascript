15053f63f40d5b5c69d02e1e9d031f3898139dac
Sylvain UTARD <sylvain.utard@gmail.com>
2014-11-28 09:33:01 +0100

    Default TLD is now .net

--- dist/algoliasearch.js
+++ dist/algoliasearch.js
@@ -70,7 +70,7 @@ var AlgoliaSearch = function(applicationID, apiKey, methodOrOptions, resolveDNS,
     this.jsonp = null;
 
     var method;
-    var tld = 'io';
+    var tld = 'net';
     if (typeof methodOrOptions === 'string') { // Old initialization
         method = methodOrOptions;
     } else {
@@ -100,11 +100,19 @@ var AlgoliaSearch = function(applicationID, apiKey, methodOrOptions, resolveDNS,
     }
     // If hosts is undefined, initialize it with applicationID
     if (this._isUndefined(hosts)) {
-        hosts = [
-            this.applicationID + '-1.algolia.' + tld,
-            this.applicationID + '-2.algolia.' + tld,
-            this.applicationID + '-3.algolia.' + tld
-        ];
+        if (tld === 'net') {
+            hosts = [
+                '1.' + this.applicationID + '.algolia.net',
+                '2.' + this.applicationID + '.algolia.net',
+                '3.' + this.applicationID + '.algolia.net'
+            ];
+        } else {
+            hosts = [
+                this.applicationID + '-1.algolia.' + tld,
+                this.applicationID + '-2.algolia.' + tld,
+                this.applicationID + '-3.algolia.' + tld
+            ];
+        }
     }
     // detect is we use http or https
     this.host_protocol = 'http://';
