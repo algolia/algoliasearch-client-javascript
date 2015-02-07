ccd5be35b38e31f58c62f2793afdd3772b93a522
vvo <vincent@zeroload.net>
2015-02-07 22:30:08 +0100

    remove unused grunt-contrib-sed

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,6 @@ module.exports = function(grunt) {
   // ----------
 
   grunt.loadNpmTasks('grunt-sed');
-  grunt.loadNpmTasks('grunt-contrib-sed');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
