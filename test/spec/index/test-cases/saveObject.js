63a77bd992407185beec98ace25fc44e87b8ff7f
vvo <vincent@zeroload.net>
2015-02-24 12:41:33 +0100

    test index.saveObject()

---
+++ test/spec/index/test-cases/saveObject.js
@@ -0,0 +1,20 @@
+var clone = require('lodash-compat/lang/clone');
+
+var object = {
+  objectID: 'dawg IE!',
+  yaw: 'partial'
+};
+
+module.exports = {
+  object: 'index',
+  methodName: 'saveObject',
+  testName: 'index.saveObject(object, cb)',
+  callArguments: [object],
+  expectedRequest: {
+    method: 'PUT',
+    body: clone(object),
+    URL: {
+      pathname: '/1/indexes/%s/' + encodeURIComponent(object.objectID)
+    }
+  }
+};
