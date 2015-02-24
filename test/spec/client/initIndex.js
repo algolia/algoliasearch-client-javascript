eb6b9a05065ed632cfd6b87d36bc0912ed084dd0
vvo <vincent@zeroload.net>
2015-02-24 17:20:35 +0100

    use compatible lodash-compat.bind

--- test/spec/client/initIndex.js
+++ test/spec/client/initIndex.js
@@ -4,11 +4,13 @@ test('client.initIndex()', function(t) {
   t.plan(1);
 
   var AlgoliaSearch = require('algoliasearch');
+  var bind = require('lodash-compat/function/bind');
+
   var getCredentials = require('../../utils/get-credentials');
 
   var credentials = getCredentials();
 
   var client = new AlgoliaSearch(credentials.applicationID, credentials.searchOnlyAPIKey);
 
-  t.doesNotThrow(client.initIndex.bind(client, credentials.indexName));
+  t.doesNotThrow(bind(client.initIndex, client, credentials.indexName));
 });
