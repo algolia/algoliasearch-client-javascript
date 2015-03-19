5ef3cc4eaed74db7e6fab22f6a074de8b6fef225
vvo <vincent.voyer@algolia.com>
2015-03-19 17:44:30 +0100

    lint fix

--- test/spec/request-strategy/no-JSONP-when-4xx-or-1xx.js
+++ test/spec/request-strategy/no-JSONP-when-4xx-or-1xx.js
@@ -5,16 +5,13 @@ test('Request strategy does not fallback to JSONP', function(t) {
   noJSONP(t, 101);
 });
 
-function noJSONP(t, statusCode) {
-  t.test('=> when statusCode is ' + statusCode, function(t) {
+function noJSONP(mainTest, statusCode) {
+  mainTest.test('=> when statusCode is ' + statusCode, function(t) {
     t.plan(3);
     var fauxJax = require('faux-jax');
-    var parse = require('url-parse');
-    var sinon = require('sinon');
 
     var createFixture = require('../../utils/create-fixture');
 
-    var currentURL = parse(location.href);
     var fixture = createFixture({
       // if JSONP was used, this would call this
       // url and we would get a response
@@ -34,11 +31,11 @@ function noJSONP(t, statusCode) {
 
       t.ok(err instanceof Error, 'err is an Error');
       t.equal(err.message, 'No JSONP when ' + statusCode, 'Error message matches');
-    };
+    }
 
     fauxJax.install();
 
-    index.search('No JSONP when ' +  statusCode, searchCallback);
+    index.search('No JSONP when ' + statusCode, searchCallback);
 
     fauxJax.requests[0]
       .respond(statusCode, {}, JSON.stringify({
