95d043c9050a3d5dde5cbfb6cf322e214558109a
vvo <vincent.voyer@algolia.com>
2015-03-21 01:35:47 +0100

    more file size reduction

--- build.sh
+++ build.sh
@@ -10,17 +10,17 @@ echo 'Building algoliasearch-client-js'
 echo '..Browserify'
 
 # cannot use a loop, bundles are different (--standalone)
-browserify index.js --standalone algoliasearch > dist/algoliasearch.js
-browserify src/algoliasearch.angular.js > dist/algoliasearch.angular.js
-browserify src/algoliasearch.jquery.js > dist/algoliasearch.jquery.js
+browserify -p bundle-collapser/plugin index.js --standalone algoliasearch > dist/algoliasearch.js
+browserify -p bundle-collapser/plugin src/algoliasearch.angular.js > dist/algoliasearch.angular.js
+browserify -p bundle-collapser/plugin src/algoliasearch.jquery.js > dist/algoliasearch.jquery.js
 
-echo '..Uglify'
+echo '..Minifying'
 
 bundles=( algoliasearch algoliasearch.angular algoliasearch.jquery )
 
 for bundle in "${bundles[@]}"
 do
-  uglifyjs dist/"$bundle".js --mangle --compress=warnings=false > dist/"$bundle".min.js
+  ccjs dist/"$bundle".js > dist/"$bundle".min.js
 done
 
 echo '..Prepend license'
