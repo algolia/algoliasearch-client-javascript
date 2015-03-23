53c73269557f51c6599376f2efa8eb7442104713
vvo <vincent.voyer@algolia.com>
2015-03-23 15:41:43 +0100

    Revert "fix syntax (facetStats to facets_stats)"
    This reverts commit 11cecaa349869b2054b4affdfd60398cad41bca1.
    
    This commit was done after the V3 branch started, we should have
    done it in a V2 branch. Did not thought about it before, will
    be cherry picked in the algoliasearch helper by @bobylito

--- src/algoliasearch.helper.js
+++ src/algoliasearch.helper.js
@@ -352,7 +352,7 @@
         }
         var aggregatedAnswer = content.results[0];
         aggregatedAnswer.disjunctiveFacets = aggregatedAnswer.disjunctiveFacets || {};
-        aggregatedAnswer.facets_stats = aggregatedAnswer.facets_stats || {};
+        aggregatedAnswer.facetStats = aggregatedAnswer.facetStats || {};
         // create disjunctive facets from facets (disjunctive facets without refinements)
         for (var facet in unusedDisjunctiveFacets) {
           if (aggregatedAnswer.facets[facet] && !aggregatedAnswer.disjunctiveFacets[facet]) {
@@ -379,13 +379,9 @@
           }
           // aggregate the disjunctive facets stats
           for (var stats in content.results[i + 1].facets_stats) {
-            aggregatedAnswer.facets_stats[stats] = content.results[i + 1].facets_stats[stats];
+            aggregatedAnswer.facetStats[stats] = content.results[i + 1].facets_stats[stats];
           }
         }
-
-        // Backward compatibility
-        aggregatedAnswer.facetStats = aggregatedAnswer.facets_stats;
-
         // add the excludes
         for (var exclude in self.excludes) {
           if (self.excludes[exclude]) {
