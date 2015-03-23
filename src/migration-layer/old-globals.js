da0e57cec7c0022e5fd9ab94b8425631e627e539
vvo <vincent.voyer@algolia.com>
2015-03-23 15:33:49 +0100

    remove unused eslint rules

--- src/migration-layer/old-globals.js
+++ src/migration-layer/old-globals.js
@@ -1,3 +1,6 @@
+/*global AlgoliaExplainResults:true*/
+/*eslint no-unused-vars: [2, {"vars": "local"}]*/
+
 module.exports = oldGlobals;
 
 function oldGlobals() {
@@ -5,17 +8,15 @@ function oldGlobals() {
     'You are trying to use a new version of the AlgoliaSearch JavaScript client with an old notation.' +
     '\nPlease read our migration guide at https://github.com/algolia/algoliasearch-client-js/wiki/Migration-guide-from-2.x.x-to-3.x.x';
 
-  // cannot dynamically add these properties to window, fails on IE old versions
-  /*global AlgoliaSearch:true,AlgoliaSearchHelper:true,AlgoliaExplainResults:true*/
-  /*eslint no-unused-vars: [2, {"vars": "local"}]*/
-  AlgoliaSearch = function() {
+  global.AlgoliaSearch = function() {
     throw new Error(message);
   };
 
-  AlgoliaSearchHelper = function() {
+  global.AlgoliaSearchHelper = function() {
     throw new Error(message);
   };
 
+  // cannot use window.AlgoliaExplainResults on old IEs, dunno why
   AlgoliaExplainResults = function() {
     throw new Error(message);
   };
