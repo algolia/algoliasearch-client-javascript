517eba498e76540ea1bf9f6f4376ca33487dc6a8
vvo <vincent.voyer@algolia.com>
2015-04-14 09:46:32 +0200

    shuffle() no more needed

--- src/AlgoliaSearch.js
+++ src/AlgoliaSearch.js
@@ -1459,25 +1459,6 @@ function map(arr, fn){
   return ret;
 }
 
-// extracted from http://bost.ocks.org/mike/shuffle/
-function shuffle(array) {
-  var m = array.length, t, i;
-
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-
-  return array;
-}
-
 function prepareHost(protocol) {
   return function prepare(host) {
     return protocol + '//' + host.toLowerCase();
