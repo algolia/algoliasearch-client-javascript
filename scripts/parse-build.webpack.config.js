4db6bb5ced03abc197d7c531a199fe089557284a
vvo <vincent.voyer@algolia.com>
2015-04-18 02:01:14 +0200

    Parse build process
    Parse module is working
    
    Notes:
    - we use webpack --target node to build a standalone JS file
    - we fallback to browser implem for some modules otherwise they would
    require('$core-nodejs-module'), not supported by parse
    - we use a modified setTimeout, was unable to not let it leak in main
    src/AlgoliaSearch. Parse does not have setTimeout nor does it allows
    to override it globally
    - Parse cloud has its owns promises, good boy!

---
+++ scripts/parse-build.webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '..', 'src/server/builds/parse.js'),
+  output: {
+      path: path.join(__dirname, '..', 'dist'),
+      filename: 'algoliasearch.parse.js',
+      libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  resolve: {
+    alias: {
+      debug: 'debug/browser.js',
+      inherits: 'inherits/inherits_browser.js',
+      'util-deprecate': 'util-deprecate/browser.js'
+    }
+  },
+  node: {
+    process: 'mock'
+  },
+  module: {
+    loaders: [{
+      test: /\.json$/,
+      loader: 'json'
+    }]
+  }
+};
---
+++ src/server/builds/parse.js
@@ -0,0 +1,122 @@
+// This is the Parse entry point
+// See https://www.parse.com/docs/cloud_code_guide#cloud_code
+module.exports = algoliasearch;
+
+process.env = {};
+/*global global:true*/
+global = {};
+
+var debug = require('debug')('algoliasearch:parse');
+
+var inherits = require('inherits');
+
+var AlgoliaSearch = require('../../AlgoliaSearch');
+
+debug('loaded the Parse client');
+
+function algoliasearch(applicationID, apiKey, opts) {
+  var extend = require('extend');
+  opts = extend(true, {}, opts) || {};
+
+  if (opts.protocol === undefined) {
+    opts.protocol = 'https:';
+  }
+
+  opts._setTimeout = _setTimeout;
+
+  opts._ua = algoliasearch.ua;
+  opts._useCache = false;
+
+  return new AlgoliaSearchParse(applicationID, apiKey, opts);
+}
+
+algoliasearch.version = require('../../version.json');
+algoliasearch.ua = 'Algolia for Parse ' + algoliasearch.version;
+
+function AlgoliaSearchParse() {
+  // call AlgoliaSearch constructor
+  AlgoliaSearch.apply(this, arguments);
+}
+
+inherits(AlgoliaSearchParse, AlgoliaSearch);
+
+AlgoliaSearchParse.prototype._request = function(rawUrl, opts) {
+  /*global Parse*/
+  var extend = require('extend');
+  var promise = new Parse.Promise();
+
+  debug('url: %s, opts: %j', rawUrl, opts);
+
+  var parseReqOpts = {
+    url: rawUrl,
+    headers: extend([], opts.headers),
+    method: opts.method,
+    success: success,
+    error: error
+  };
+
+  if (opts.body !== undefined) {
+    parseReqOpts.headers['content-type'] = 'application/json';
+    parseReqOpts.body = opts.body;
+  }
+
+  Parse.Cloud.httpRequest(parseReqOpts);
+
+  function error(res) {
+    debug('Error: %j  - %s %j', res, rawUrl, opts);
+
+    // we still resolve, bc Parse does not distinguish network errors
+    // from 400/500 statuses
+    promise.resolve({
+      statusCode: res.status,
+      body: res.data
+    });
+  }
+
+  function success(res) {
+    debug('Success: %j  - %s %j', res, rawUrl, opts);
+
+    promise.resolve({
+      statusCode: res.status,
+      body: res.data
+    });
+  }
+
+  return promise;
+};
+
+AlgoliaSearchParse.prototype._promise = {
+  reject: function(val) {
+    return Parse.Promise.error(val);
+  },
+  resolve: function(val) {
+    return Parse.Promise.as(val);
+  },
+  delay: function(ms) {
+    var promise = new Parse.Promise();
+
+    _setTimeout(promise.resolve.bind(promise), ms);
+
+    return promise;
+  }
+};
+
+// no need for .destroy() in parse client
+// dunno about the state of keepalive in parse cloud http request implementation
+delete AlgoliaSearchParse.prototype.destroy;
+
+// There's no setTimeout in Parse cloud, but we have nextTick
+function _setTimeout(fn, ms) {
+  var start = Date.now();
+
+  process.nextTick(fakeSetTimeout);
+
+  function fakeSetTimeout() {
+    if (Date.now() < start + ms) {
+      process.nextTick(fakeSetTimeout);
+      return;
+    }
+
+    fn();
+  }
+}
