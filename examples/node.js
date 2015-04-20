549d9bbb10b8e176dff26bf095542e1eea65418a
vvo <vincent.voyer@algolia.com>
2015-04-20 11:48:09 +0200

    add nodejs and parse examples

---
+++ examples/node.js
@@ -0,0 +1,17 @@
+// node examples/node.js
+var algoliasearch = require('../');
+var client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+var index = client.initIndex('contacts');
+
+index.search('Atlenta', function(err, results) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('We got `' + results.nbHits + '` results');
+  console.log('Here is the first one: ', results.hits[0]);
+
+  // call client.destroy() this when you need to stop the node.js client
+  // it will release any keepalived connection
+  client.destroy();
+});
---
+++ examples/parse.js
@@ -0,0 +1,15 @@
+// put this file in your `cloud/` folder of your Parse application
+// also copy the `dist/algoliasearch.parse.js` to `cloud/algoliasearch.parse.js`
+var algoliasearch = require('cloud/algoliasearch.parse.js');
+var client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+var index = client.initIndex('contacts');
+
+Parse.Cloud.define("hello", function(request, response) {
+  index.search('Atlenta', function(err, results) {
+    if (err) {
+      throw err;
+    }
+
+    response.success('We got ' + results.nbHits + ' results');
+  });
+});
