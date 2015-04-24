1e17d81dec8782af0e080e7d880a6d47f4bc4c3f
vvo <vincent.voyer@algolia.com>
2015-04-24 13:53:55 +0200

    enhance timeout test

--- test/spec/node/timeout.js
+++ test/spec/node/timeout.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 
 test('Default request timeout is approx 3s', function(t) {
-  t.plan(2);
+  t.plan(3);
   var fauxJax = require('faux-jax');
 
   var createFixture = require('../../utils/create-fixture');
@@ -15,10 +15,15 @@ test('Default request timeout is approx 3s', function(t) {
     t.error(err, 'We got two requests');
 
     var elapsed = Math.round((Date.now() - start) / 1000);
-    requests[1].respond(200, {}, '{}');
+    requests[1].respond(200, {}, '{"timeout": "ok"}');
     fauxJax.restore();
     t.equal(elapsed, 3, 'Default request READ timeout is approximately 3s');
   });
 
-  index.search('dsads');
+  index.search('dsads').then(function(content) {
+    t.deepEqual(content, {
+      timeout: 'ok'
+    }, 'content matches');
+
+  }).catch(t.fail.bind(t));
 });
