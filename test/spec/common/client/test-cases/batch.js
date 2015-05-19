f304f06fbdc1f75d9914d1bed3adb1632a62345c
vvo <vincent.voyer@algolia.com>
2015-05-19 10:34:01 +0200

    feat: client.batch()
    perform multiple write operations accross indexes

---
+++ test/spec/common/client/test-cases/batch.js
@@ -0,0 +1,41 @@
+module.exports = {
+  only: true,
+  testName: 'client.batch(operations, cb)',
+  object: 'client',
+  methodName: 'batch',
+  callArguments: [[{
+    action: 'addObject',
+    indexName: 'clients',
+    body: {
+      name: 'Bill'
+    }
+  }, {
+    action: 'udpateObject',
+    indexName: 'fruits',
+    body: {
+      objectID: '29138',
+      name: 'banana'
+    }
+  }]],
+  action: 'write',
+  expectedRequest: {
+    method: 'POST',
+    URL: {pathname: '/1/indexes/*/batch'},
+    body: {
+      requests: [{
+        action: 'addObject',
+        indexName: 'clients',
+        body: {
+          name: 'Bill'
+        }
+      }, {
+        action: 'udpateObject',
+        indexName: 'fruits',
+        body: {
+          objectID: '29138',
+          name: 'banana'
+        }
+      }]
+    }
+  }
+};
