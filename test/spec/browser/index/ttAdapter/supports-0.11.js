220f1538e39d68dd1a519fa1f747cb19799fd745
vvo <vincent.voyer@algolia.com>
2015-06-02 16:32:55 +0200

    fix: support typeahead 0.11
    + update example
    + added a test
    
    fixes #105

---
+++ test/spec/browser/index/ttAdapter/supports-0.11.js
@@ -0,0 +1,34 @@
+var test = require('tape');
+
+test('index.ttAdapter(cb) supports typeahead 0.11', function(t) {
+  t.plan(1);
+
+  var fauxJax = require('faux-jax');
+
+  var createFixture = require('../../../../utils/create-fixture');
+  var fixture = createFixture();
+
+  var index = fixture.index;
+
+  var ttAdapter = index.ttAdapter();
+
+  fauxJax.install();
+
+  fauxJax.on('request', function(req) {
+    fauxJax.restore();
+    req.respond(200, {}, JSON.stringify(fakeResponse));
+  });
+
+  var fakeResponse = {
+    hits: [1, 2, 3]
+  };
+
+  // typeAhead 0.11 sends 3 parameters
+  ttAdapter('a search', undefined, function(actualHits) {
+    t.deepEqual(
+      actualHits,
+      fakeResponse.hits,
+      'We received some hits'
+    );
+  });
+});
