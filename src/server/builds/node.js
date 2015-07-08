f0d4578a7453805ca1167d691e2eb9bbe578e800
vvo <vincent.voyer@algolia.com>
2015-07-08 13:26:57 +0200

    fix: do not rely on any "smart" port computing
    see https://github.com/TooTallNate/node-https-proxy-
    agent/issues/7#issuecomment-119539690

--- src/server/builds/node.js
+++ src/server/builds/node.js
@@ -97,8 +97,17 @@ AlgoliaSearchNodeJS.prototype._request = function(rawUrl, opts) {
     var req;
 
     if (parsedUrl.protocol === 'https:') {
+      // we do not rely on any "smart" port computing by either node.js
+      // or a custom http agent, because: https://github.com/TooTallNate/node-https-proxy-agent/issues/7#issuecomment-119539690
+      if (requestOptions.port === undefined) {
+        requestOptions.port = 443;
+      }
       req = https.request(requestOptions);
     } else {
+      // same reason to set the port as `https:`
+      if (requestOptions.port === undefined) {
+        requestOptions.port = 80;
+      }
       req = http.request(requestOptions);
     }
 
