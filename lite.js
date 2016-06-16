002601c1b40aa23c5d179266639e72a5333ffd7b
vvo <vincent.voyer@algolia.com>
2016-06-16 16:13:23 +0200

    fix(nodejs): allow universal lite applications
    require('algoliasearch/lite') should work to
    facilitate universal applications builds using
    the lite build on frontend
    
    fixes #283

--- lite.js
+++ lite.js
@@ -1,4 +1,7 @@
 'use strict';
 
-// Alias for the react native build of the client.
-module.exports = require('./src/browser/builds/algoliasearchLite.js');
+// For Node.js users, you will get the normal build (no lite)
+// For browser users, this will be mapped to src/browser/builds/algoliasearchLite.js
+// when used through browserify or webpack (see package.json browser field)
+// This is done to ease universal applications (https://github.com/algolia/algoliasearch-client-js/issues/283)
+module.exports = require('./index.js');
