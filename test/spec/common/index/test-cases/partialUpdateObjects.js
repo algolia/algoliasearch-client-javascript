9f315ad559c3007506ad291bdf06fdc87d97b978
Vincent Voyer <wouaren@gmail.com>
2017-05-18 13:52:53 +0200

    fix(partialUpdateObjects): allow createIfNotExists (#552)
    Much like partialUpdateObject, allow createIfNotExists
    
    fixes #551

--- test/spec/common/index/test-cases/partialUpdateObjects.js
+++ test/spec/common/index/test-cases/partialUpdateObjects.js
@@ -8,7 +8,7 @@ var objects = [{
   second: 'object'
 }];
 
-module.exports = {
+module.exports = [{
   object: 'index',
   methodName: 'partialUpdateObjects',
   testName: 'index.partialUpdateObjects(objects, cb)',
@@ -29,4 +29,25 @@ module.exports = {
       pathname: '/1/indexes/%s/batch'
     }
   }
-};
+}, {
+  object: 'index',
+  methodName: 'partialUpdateObjects',
+  testName: 'index.partialUpdateObjects(objects, false, cb)',
+  callArguments: [objects, false],
+  action: 'write',
+  expectedRequest: {
+    method: 'POST',
+    body: {
+      requests: [{
+        action: 'partialUpdateObjectNoCreate',
+        body: clone(objects[0])
+      }, {
+        action: 'partialUpdateObjectNoCreate',
+        body: clone(objects[1])
+      }]
+    },
+    URL: {
+      pathname: '/1/indexes/%s/batch'
+    }
+  }
+}];
