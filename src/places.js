58efc4eae48a8e33b603ac8e5f05807071fd5998
vvo <vincent.voyer@algolia.com>
2017-06-08 11:29:28 +0200

    feat(places): add getObject

--- src/places.js
+++ src/places.js
@@ -24,6 +24,14 @@ function createPlacesClient(algoliasearch) {
     var client = algoliasearch(appID, apiKey, opts);
     var index = client.initIndex('places');
     index.search = buildSearchMethod('query', '/1/places/query');
+    index.getObject = function(objectID, callback) {
+      return this.as._jsonRequest({
+        method: 'GET',
+        url: '/1/places/' + encodeURIComponent(objectID),
+        hostType: 'read',
+        callback: callback
+      });
+    };
     return index;
   };
 }
