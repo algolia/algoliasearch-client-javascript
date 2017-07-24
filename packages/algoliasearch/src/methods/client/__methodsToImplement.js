10d021418359b9f2a6fed92eb48f8a83bbc0a284
Haroen Viaene <hello@haroen.me>
2017-07-24 16:34:40 +0200

    add methods to implement

---
+++ packages/algoliasearch/src/methods/client/__methodsToImplement.js
@@ -0,0 +1,31 @@
+const clientMethods = [
+  'addApiKey',
+  'batch',
+  'clearCache',
+  'copyIndex',
+  'deleteApiKey',
+  'deleteIndex',
+  'getApiKey',
+  'getLogs',
+  'getTimeouts',
+  'initIndex',
+  'listApiKeys',
+  'listIndexes',
+  'moveIndex',
+  'search',
+  'sendQueriesBatch',
+  'setExtraHeader',
+  'setRequestTimeout',
+  'setSecurityTags',
+  'setTimeouts',
+  'updateApiKey',
+];
+
+const nodeOnlyClientMethods = [
+  'destroy',
+  'disableRateLimitForward',
+  'disableSecuredAPIKey',
+  'enableRateLimitForward',
+  'generateSecuredApiKey',
+  'useSecuredAPIKey',
+];
