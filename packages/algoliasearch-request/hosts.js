d6ce7e2d149f70280c8b419b62f9b5b1933f5b02
Haroen Viaene <hello@haroen.me>
2017-07-31 20:54:25 +0200

    hosts thingy

--- packages/algoliasearch-request/hosts.js
+++ packages/algoliasearch-request/hosts.js
@@ -1,7 +1,15 @@
 // @flow
 import type { AppId } from '../algoliasearch/src/types';
 
-type Hosts = {| read: string[], write: string[] |};
+export type Hosts = {|
+  read: string[],
+  write: string[],
+|};
+export type Timeouts = {|
+  connect: number,
+  read: number,
+  write: number,
+|};
 
 function computeRegularHosts(appId: AppId): Hosts {
   const readWriteHosts = [
@@ -16,26 +24,53 @@ function computeRegularHosts(appId: AppId): Hosts {
   };
 }
 
-export default class RequestHosts {
-  hosts: Hosts;
-  readIndex: Number;
-  writeIndex: Number;
+const computeRegularTimeouts = () => ({
+  connect: 1 * 1000, // 500ms connect is GPRS latency
+  read: 2 * 1000,
+  write: 30 * 1000,
+});
 
-  constructor(appId: AppId, extraHosts: Hosts) {
+type Args = {|
+  appId: AppId,
+  extraHosts?: Hosts,
+  timeouts?: Timeouts,
+|};
+
+export default class requestHosts {
+  hosts: Hosts;
+  timeouts: Timeouts;
+
+  constructor({ appId, extraHosts, timeouts: extraTimeouts }: Args) {
     const regularHosts = computeRegularHosts(appId);
     this.hosts = {
       read: [...regularHosts.read, ...extraHosts.read],
       write: [...regularHosts.write, ...extraHosts.write],
     };
-    this.readIndex = 0;
-    this.writeIndex = 0;
-  }
-  // to fix
-  get read() {
-    return this.hosts.read[this.readIndex];
+
+    const regularTimeouts = computeRegularTimeouts();
+    this.timeouts = {
+      ...regularTimeouts,
+      ...extraTimeouts,
+    };
   }
 
-  get write() {
-    return this.hosts.write[this.writeIndex];
+  *readHost() {
+    const reset = yield {
+      host: this.hosts.read.shift(),
+      timeout: this.timeouts.timeouts.read,
+    };
+    if (reset === true) {
+      constructor();
+    }
+  }
+
+  *writeHost() {
+    const reset = yield {
+      host: this.hosts.write.shift(),
+      timeout: this.timeouts.timeouts.write,
+    };
+    if (reset === true) {
+      constructor();
+    }
   }
 }
