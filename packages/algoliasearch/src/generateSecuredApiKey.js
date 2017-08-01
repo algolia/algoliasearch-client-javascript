de45c443af780ca6682998fbfea9dc8e3e914363
Haroen Viaene <hello@haroen.me>
2017-08-01 13:25:15 +0200

    stringify objects for secured API key

--- packages/algoliasearch/src/generateSecuredApiKey.js
+++ packages/algoliasearch/src/generateSecuredApiKey.js
@@ -1,18 +1,34 @@
 // @flow
 import crypto from 'crypto';
+import { stringify } from 'querystring';
 import type { ApiKey, SearchParameters } from './types';
+
+type Timestamp = number;
+
+type ApiKeyParameters = {|
+  validUntil: Timestamp,
+  restrictIndices: string,
+  userToken: string,
+  restrictSources: string,
+|};
+
+export type SecuredApiParameters = {|
+  ...SearchParameters,
+  ...ApiKeyParameters,
+|};
 
 export default function generateSecuredApiKey({
   privateApiKey,
   queryParameters,
 }: {
   privateApiKey: ApiKey,
-  queryParameters: SearchParameters,
+  queryParameters: SecuredApiParameters,
 }) {
+  const params = stringify(queryParameters);
   const securedKey = crypto
     .createHmac('sha256', privateApiKey)
-    .update(queryParameters)
+    .update(params)
     .digest('hex');
 
-  return new Buffer(securedKey + queryParameters).toString('base64');
+  return new Buffer(securedKey + params).toString('base64');
 }
