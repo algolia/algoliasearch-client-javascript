6d941a1429b716a9c7106c4b2707b43fa5eb07ce
Haroen Viaene <hello@haroen.me>
2017-08-02 13:00:22 +0200

    some changes

--- packages/algoliasearch-request/hosts.js
+++ packages/algoliasearch-request/hosts.js
@@ -5,12 +5,6 @@ export type Hosts = {|
   read: string[],
   write: string[],
 |};
-export type Timeouts = {|
-  connect: number,
-  read: number,
-  write: number,
-|};
-
 function computeRegularHosts(appId: AppId): Hosts {
   const readWriteHosts = [
     `${appId}-1.algolianet.com`,
@@ -24,11 +18,18 @@ function computeRegularHosts(appId: AppId): Hosts {
   };
 }
 
-const computeRegularTimeouts = () => ({
-  connect: 1 * 1000, // 500ms connect is GPRS latency
-  read: 2 * 1000,
-  write: 30 * 1000,
-});
+export type Timeouts = {
+  connect: number,
+  read: number,
+  write: number,
+};
+function computeRegularTimeouts(): Timeouts {
+  return {
+    connect: 1 * 1000, // 500ms connect is GPRS latency
+    read: 2 * 1000,
+    write: 30 * 1000,
+  };
+}
 
 type Args = {|
   appId: AppId,
@@ -39,12 +40,17 @@ type Args = {|
 export default class requestHosts {
   hosts: Hosts;
   timeouts: Timeouts;
+  appId: AppId;
 
   constructor({ appId, extraHosts, timeouts: extraTimeouts }: Args) {
+    this.appId = appId;
+
     const regularHosts = computeRegularHosts(appId);
+    const { read = [], write = [] } = extraHosts;
+
     this.hosts = {
-      read: [...regularHosts.read, ...extraHosts.read],
-      write: [...regularHosts.write, ...extraHosts.write],
+      read: [...regularHosts.read, ...read],
+      write: [...regularHosts.write, ...write],
     };
 
     const regularTimeouts = computeRegularTimeouts();
@@ -54,23 +60,37 @@ export default class requestHosts {
     };
   }
 
-  *readHost() {
-    const reset = yield {
-      host: this.hosts.read.shift(),
-      timeout: this.timeouts.timeouts.read,
-    };
-    if (reset === true) {
-      constructor();
+  getHost({ type }: { type: 'read' | 'write' }): string {
+    if (type !== 'read' && type !== 'write') {
+      throw new Error(
+        `You requested a host of type ${type}, but only 'read' or 'write' is a valid type.`
+      );
     }
+    const hosts = this.hosts[type];
+
+    if (hosts.length === 1) {
+      const regularHosts = computeRegularHosts(this.appId)[type];
+      this.hosts[type] = [...hosts, ...regularHosts];
+    }
+
+    return hosts.shift();
   }
 
-  *writeHost() {
-    const reset = yield {
-      host: this.hosts.write.shift(),
-      timeout: this.timeouts.timeouts.write,
-    };
-    if (reset === true) {
-      constructor();
-    }
+  getTimeout({
+    retry,
+    type,
+  }: {
+    retry: number,
+    type: $Keys<Timeouts>,
+  }): number {
+    // return switch (type) {
+    //   case 'connect':
+
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    return retry * this.timeouts[type];
   }
 }
