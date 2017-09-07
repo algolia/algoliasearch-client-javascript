dc4cba3296e1a8dd79e59c8f566bc5f6bcefa7e8
Haroen Viaene <hello@haroen.me>
2017-09-07 09:29:07 +0200

    implement parseOptions

--- packages/algoliasearch-request/parseOptions.js
+++ packages/algoliasearch-request/parseOptions.js
@@ -9,6 +9,55 @@ type Translation = {
 const translationTable: Translation = {
   forwardedFor: 'X-Forwarded-For',
   userAgent: 'X-Algolia-Agent',
+  userId: 'X-Algolia-UserId',
 };
 
-console.log(translationTable);
+const alwaysHeader: Array<string> = [];
+
+export default function parseOptions(options: RequestOptions) {
+  const keys: $Keys<RequestOptions> = Object.keys(options);
+
+  return keys.reduce(
+    (acc, key) => {
+      if (key === 'extraHeaders') {
+        return {
+          ...acc,
+          headers: {
+            ...acc.headers,
+            ...options.extraHeaders,
+          },
+        };
+      }
+      if (key === 'timeouts') {
+        return {
+          ...acc,
+          timeouts: {
+            ...acc.timeouts,
+            ...options.timeouts,
+          },
+        };
+      }
+      if (alwaysHeader.contains(key)) {
+        return {
+          ...acc,
+          headers: {
+            ...acc.headers,
+            [translationTable[key]]: options[key],
+          },
+        };
+      }
+      return {
+        ...acc,
+        queryStringOrBody: {
+          ...acc.queryStringOrBody,
+          [key]: options[key],
+        },
+      };
+    },
+    {
+      queryStringOrBody: {},
+      headers: {},
+      timeouts: {},
+    }
+  );
+}
