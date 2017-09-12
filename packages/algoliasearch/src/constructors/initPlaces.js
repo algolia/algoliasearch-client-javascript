eb1f49bec9f27a0ce618c5fb75ceaef66a2c9c2b
Haroen Viaene <hello@haroen.me>
2017-09-12 17:11:18 +0200

    correct places initialisation

--- packages/algoliasearch/src/constructors/initPlaces.js
+++ packages/algoliasearch/src/constructors/initPlaces.js
@@ -5,26 +5,38 @@ import * as placesMethods from '../methods/places';
 import { createRequester } from '../request';
 import attachParameters from './attachParameters';
 
-import type { RequestMethod, ApiKey } from '../types';
+import type { RequestMethod, RequesterOptions, ApiKey, AppId } from '../types';
 
 export default function initPlaces(
   {
     apiKey = '',
+    appId = 'places',
     requester: extraRequester,
-    ...otherArgs
+    options,
   }: {
     apiKey?: ApiKey,
+    appId?: AppId,
     requester?: RequestMethod,
+    options?: RequesterOptions,
   } = {}
 ) {
-  if (otherArgs.appId) {
+  // if there's an appId, there should also be an apiKey
+  // all other cases are invalid
+  if (
+    (apiKey !== '' && (appId === 'places' || appId === '')) ||
+    (appId !== 'places' && appId !== '' && apiKey === '')
+  ) {
+    /* eslint-disable prefer-rest-params */
     throw new Error(
-      'You passed an `appId` to initPlaces, but this only accepts `apiKey` as configuration'
+      `Credentials not valid: you gave an API key (${apiKey}), and appId (${appId}) 
+      
+      initPlaces(${[...arguments].map(arg => JSON.stringify(arg)).join(',')})`
     );
+    /* eslint-enable prefer-rest-params */
   }
   const requester = extraRequester
     ? extraRequester
-    : createRequester({ appId: 'places', apiKey });
+    : createRequester({ appId, apiKey, options });
 
   return {
     ...attachParameters(placesMethods, { meta: { requester } }),
--- packages/algoliasearch/src/constructors/initPlaces.test.js
+++ packages/algoliasearch/src/constructors/initPlaces.test.js
@@ -3,8 +3,11 @@ import initPlaces from './initPlaces';
 it('initPlaces throws when it has an appId', () => {
   expect(() => initPlaces()).not.toThrow();
   expect(() => initPlaces({})).not.toThrow();
-  expect(() => initPlaces({ apiKey: 'some_key' })).not.toThrow();
+  expect(() => initPlaces({ appId: 'pl-cool', apiKey: 'key' })).not.toThrow();
 
+  expect(() =>
+    initPlaces({ apiKey: 'some_key' })
+  ).toThrowErrorMatchingSnapshot();
   expect(() =>
     initPlaces({ appId: 'some_app' })
   ).toThrowErrorMatchingSnapshot();
