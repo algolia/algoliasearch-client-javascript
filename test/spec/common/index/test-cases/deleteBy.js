5c22006b6a0bf9d29d14531af4eed9d1e14799e5
Haroen Viaene <hello@haroen.me>
2017-09-29 13:32:08 +0200

    fix tests

--- test/spec/common/index/test-cases/deleteBy.js
+++ test/spec/common/index/test-cases/deleteBy.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.warn('lol it runs')
-
 module.exports = [
   {
     object: 'index',
@@ -15,7 +13,26 @@ module.exports = [
         pathname: '/1/indexes/%s/deleteByQuery',
       },
       body: {
-        params: 'query=some%22q',
+        params: 'query=some%20q',
+      },
+    },
+  },
+  {
+    object: 'index',
+    methodName: 'deleteBy',
+    testName: 'index.deleteBy({query}, cb)',
+    callArguments: [
+      { query: 'some q', facetFilters: 'category:Book,author:John%20Doe' },
+    ],
+    action: 'write',
+    expectedRequest: {
+      method: 'POST',
+      URL: {
+        pathname: '/1/indexes/%s/deleteByQuery',
+      },
+      body: {
+        params:
+          'query=some%20q&facetFilters=category%3ABook%2Cauthor%3AJohn%2520Doe',
       },
     },
   },
