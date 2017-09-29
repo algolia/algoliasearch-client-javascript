cc032b99f14ad54e00e7ce4495c5504a47e349c7
Haroen Viaene <hello@haroen.me>
2017-09-29 13:12:25 +0200

    just single test

--- test/spec/common/index/test-cases/deleteBy.js
+++ test/spec/common/index/test-cases/deleteBy.js
@@ -6,22 +6,6 @@ module.exports = [
   {
     object: 'index',
     methodName: 'deleteBy',
-    testName: 'index.deleteBy(undefined, cb)',
-    callArguments: [],
-    action: 'write',
-    expectedRequest: {
-      method: 'POST',
-      URL: {
-        pathname: '/1/indexes/%s/deleteByQuery',
-      },
-      body: {
-        params: '',
-      },
-    },
-  },
-  {
-    object: 'index',
-    methodName: 'deleteBy',
     testName: 'index.deleteBy({query}, cb)',
     callArguments: [{ query: 'some q' }],
     action: 'write',
@@ -32,64 +16,6 @@ module.exports = [
       },
       body: {
         params: 'query=some%22q',
-      },
-    },
-  },
-  {
-    object: 'index',
-    methodName: 'deleteBy',
-    testName: 'index.deleteBy({query, filters}, cb)',
-    // this test should fail but it doesn't, so I don't trust the new tests 😭
-    callArguments: [{ query: '', filters: 'color:red', ridiculous:'mistake' }],
-    action: 'write',
-    expectedRequest: {
-      method: 'POST',
-      URL: {
-        pathname: '/1/indexes/%s/deleteByQuery',
-      },
-      body: {
-        params: 'query=&filters=color%3Ared',
-      },
-    },
-  },
-  {
-    object: 'index',
-    methodName: 'deleteBy',
-    testName: 'index.deleteBy(queryParameters)',
-    callArguments: [
-      {
-        some: 'thing',
-      },
-    ],
-    action: 'write',
-    expectedRequest: {
-      method: 'POST',
-      URL: {
-        pathname: '/1/indexes/%s/deleteByQuery',
-      },
-      body: {
-        params: 'some=thing',
-      },
-    },
-  },
-  {
-    object: 'index',
-    methodName: 'deleteBy',
-    testName: 'index.deleteBy(queryParameters)',
-    callArguments: [
-      {
-        query: 'BOUHHHH!!',
-        some: 'thing',
-      },
-    ],
-    action: 'write',
-    expectedRequest: {
-      method: 'POST',
-      URL: {
-        pathname: '/1/indexes/%s/deleteByQuery',
-      },
-      body: {
-        params: 'some=thing&query=BOUHHHH!!',
       },
     },
   },
