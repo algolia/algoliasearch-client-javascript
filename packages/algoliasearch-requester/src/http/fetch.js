1d68e8035bb939c733d1073243a49bdcb610b1f9
Haroen Viaene <hello@haroen.me>
2017-10-08 16:06:29 +0200

    tiny first stab at fetch()

--- packages/algoliasearch-requester/src/http/fetch.js
+++ packages/algoliasearch-requester/src/http/fetch.js
@@ -1,1 +1,29 @@
-export default fetch;
+// @flow
+
+// todo: don't disable when doing it properly later
+/* eslint-disable */
+
+import parseOptions from '../parseOptions.js';
+import type {
+  Response,
+  RequesterArgs,
+  RequesterError
+} from 'algoliasearch-requester';
+
+export default function httpRequester({
+  body,
+  method,
+  url,
+  requestOptions,
+  timeout: originalTimeout,
+  requestType
+}: RequesterArgs): Promise<Response> {
+  const { queryStringOrBody, headers: extraHeaders, timeouts } = parseOptions(
+    requestOptions
+  );
+  const timeout = timeouts[requestType] || originalTimeout;
+
+  const controller = new FetchController();
+
+  return fetch;
+}
