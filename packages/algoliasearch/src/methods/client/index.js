157a84002604276d45935ed06986c50c435fa5a4
Haroen Viaene <hello@haroen.me>
2017-10-08 15:30:07 +0200

    correct `client` exports

--- packages/algoliasearch/src/methods/client/index.js
+++ packages/algoliasearch/src/methods/client/index.js
@@ -1,6 +1,18 @@
 // @flow
 
-export { default as batch } from './batch';
-export { default as getLogs } from './getLogs';
-export { default as listIndexes } from './listIndexes';
-export { default as search } from './search';
+export { default as addApiKey } from './addApiKey.js';
+export { default as batch } from './batch.js';
+export { default as clearIndex } from './clearIndex.js';
+export { default as copyIndex } from './copyIndex.js';
+export { default as deleteApiKey } from './deleteApiKey.js';
+export { default as deleteIndex } from './deleteIndex.js';
+export { default as false } from './false.js';
+export { default as getApiKey } from './getApiKey.js';
+export { default as getLogs } from './getLogs.js';
+export { default as index } from './index.js';
+export { default as initIndex } from './initIndex.js';
+export { default as listApiKeys } from './listApiKeys.js';
+export { default as listIndexes } from './listIndexes.js';
+export { default as moveIndex } from './moveIndex.js';
+export { default as search } from './search.js';
+export { default as updateApiKey } from './updateApiKey.js';
