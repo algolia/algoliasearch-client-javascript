15e897987dd5ecde9284e72caeef3b5bbcc8f721
Haroen Viaene <hello@haroen.me>
2017-10-14 12:38:05 -0700

    no initIndex on client

--- packages/algoliasearch/src/methods/client/index.js
+++ packages/algoliasearch/src/methods/client/index.js
@@ -8,7 +8,6 @@ export { default as deleteApiKey } from './deleteApiKey';
 export { default as deleteIndex } from './deleteIndex';
 export { default as getApiKey } from './getApiKey';
 export { default as getLogs } from './getLogs';
-export { default as initIndex } from './initIndex';
 export { default as listApiKeys } from './listApiKeys';
 export { default as listIndexes } from './listIndexes';
 export { default as moveIndex } from './moveIndex';
