b25c2d09c6fedaa9833e2b0c9f42a0fb2394f9a9
Haroen Viaene <hello@haroen.me>
2017-11-02 17:14:31 +0100

    shorthand for exporting default

--- packages/algoliasearch/src/index.js
+++ packages/algoliasearch/src/index.js
@@ -4,5 +4,4 @@ export { default as initClient } from './constructors/initClient';
 export { default as initIndex } from './constructors/initIndex';
 export { default as initPlaces } from './constructors/initPlaces';
 export { default as generateSecuredApiKey } from './generateSecuredApiKey';
-import { default as deprecateDefaultImport } from './deprecateDefaultImport';
-export default deprecateDefaultImport;
+export { default } from './deprecateDefaultImport';
