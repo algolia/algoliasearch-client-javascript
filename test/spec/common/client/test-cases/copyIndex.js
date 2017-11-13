b0519972eb8bb8a841d961ce673df5b4faf47b1f
Haroen Viaene <fingebimus@me.com>
2017-11-13 15:12:26 +0100

    feat(copyIndex): back-compatible add of scope (#618)
    copyIndex can now accept a third parameter (scope) which is an array with values 'settings', 'synonyms', 'rules'. An empty array is treated as not given, since the api otherwise returns an error, and I have the feeling that this might be what people expect.
    
    You can still have the callback as third argument if you want, or leave the third undefined and put it in the fourth position.
    
    I added new tests with a valid array, empty array and undefined.
    
    Wanted to test if callback is accepted on both position 3 and 4, but that seems to be quite hard, so for now I'll just let this be.

--- test/spec/common/client/test-cases/copyIndex.js
+++ test/spec/common/client/test-cases/copyIndex.js
@@ -1,17 +1,65 @@
 'use strict';
 
-module.exports = {
-  testName: 'client.copyIndex(from, to, cb)',
-  object: 'client',
-  methodName: 'copyIndex',
-  callArguments: ['from index', 'to index'],
-  action: 'write',
-  expectedRequest: {
-    method: 'POST',
-    URL: {pathname: '/1/indexes/from%20index/operation'},
-    body: {
-      operation: 'copy',
-      destination: 'to index'
+module.exports = [
+  {
+    testName: 'client.copyIndex(from, to, cb)',
+    object: 'client',
+    methodName: 'copyIndex',
+    callArguments: ['from index', 'to index'],
+    action: 'write',
+    expectedRequest: {
+      method: 'POST',
+      URL: {pathname: '/1/indexes/from%20index/operation'},
+      body: {
+        operation: 'copy',
+        destination: 'to index'
+      }
+    }
+  },
+  {
+    testName: 'client.copyIndex(from, to, scope)',
+    object: 'client',
+    methodName: 'copyIndex',
+    callArguments: ['from index', 'to index', ['settings']],
+    action: 'write',
+    expectedRequest: {
+      method: 'POST',
+      URL: {pathname: '/1/indexes/from%20index/operation'},
+      body: {
+        operation: 'copy',
+        destination: 'to index',
+        scope: ['settings']
+      }
+    }
+  },
+  {
+    testName: 'client.copyIndex(from, to, [])',
+    object: 'client',
+    methodName: 'copyIndex',
+    callArguments: ['from index', 'to index', []],
+    action: 'write',
+    expectedRequest: {
+      method: 'POST',
+      URL: {pathname: '/1/indexes/from%20index/operation'},
+      body: {
+        operation: 'copy',
+        destination: 'to index'
+      }
+    }
+  },
+  {
+    testName: 'client.copyIndex(from, to, undefined)',
+    object: 'client',
+    methodName: 'copyIndex',
+    callArguments: ['from index', 'to index', undefined],
+    action: 'write',
+    expectedRequest: {
+      method: 'POST',
+      URL: {pathname: '/1/indexes/from%20index/operation'},
+      body: {
+        operation: 'copy',
+        destination: 'to index'
+      }
     }
   }
-};
+];
