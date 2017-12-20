28be81e641f64aede6a1c96d7bda9a3feb905c49
Haroen Viaene <hello@haroen.me>
2017-12-20 10:39:06 +0100

    chore: add MCM methods to client

--- packages/algoliasearch/src/methods/client/index.js
+++ packages/algoliasearch/src/methods/client/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 export { default as addApiKey } from './addApiKey.js';
+export { default as assignUserID } from './assignUserID.js';
 export { default as batch } from './batch.js';
 export { default as clearIndex } from './clearIndex.js';
 export { default as copyIndex } from './copyIndex.js';
@@ -8,8 +9,13 @@ export { default as deleteApiKey } from './deleteApiKey.js';
 export { default as deleteIndex } from './deleteIndex.js';
 export { default as getApiKey } from './getApiKey.js';
 export { default as getLogs } from './getLogs.js';
+export { default as getTopUserID } from './getTopUserID.js';
 export { default as listApiKeys } from './listApiKeys.js';
+export { default as listClusters } from './listClusters.js';
 export { default as listIndexes } from './listIndexes.js';
+export { default as listUserIDs } from './listUserIDs.js';
 export { default as moveIndex } from './moveIndex.js';
+export { default as removeUserID } from './removeUserID.js';
 export { default as search } from './search.js';
+export { default as searchUserIDs } from './searchUserIDs.js';
 export { default as updateApiKey } from './updateApiKey.js';
