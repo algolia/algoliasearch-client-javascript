89a610b3035a2445e20cbc12b1e526735901ca2a
Haroen Viaene <hello@haroen.me>
2017-12-21 15:03:39 +0100

    chore: separate jest file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-commonjs */
+module.exports = {
+  moduleFileExtensions: ['js'],
+  testEnvironment: 'node',
+  displayName: 'TEST',
+  transform: {
+    '.js': 'babel-jest',
+  },
+};
