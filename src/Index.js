f23098afa17b293f1e62587b619b7c0562a59af2
Haroen Viaene <fingebimus@me.com>
2018-02-07 13:28:38 +0100

    fix(exportXXX): bind the methods (#663)
    * fix(exportXXX): bind the methods
    
    These two errors were unnoticed, because the integration tests apparently bind everything.
    
    fixes #660
    
    * chore: correct fake synonyms
    
    * chore: add highlight check
    
    * fix: correct fake number of synonyms
    
    * fix: don't return the promise if there's a callback

--- src/Index.js
+++ src/Index.js
@@ -604,7 +604,7 @@ Index.prototype.clearIndex = function(callback) {
 /*
 * Get settings of this index
 *
-* @param opts an object of options to add 
+* @param opts an object of options to add
 * @param opts.advanced get more settings like nbShards (useful for Enterprise)
 * @param callback (optional) the result callback called with two arguments
 *  error: null or Error('message')
@@ -671,6 +671,7 @@ function exportData(method, _hitsPerPage, callback) {
   return search().then(function(data) {
     if (typeof callback === 'function') {
       callback(data);
+      return undefined;
     }
     return data;
   });
@@ -682,7 +683,7 @@ function exportData(method, _hitsPerPage, callback) {
  * @param [function] callback will be called after all synonyms are retrieved
  */
 Index.prototype.exportSynonyms = function(hitsPerPage, callback) {
-  return exportData(this.searchSynonyms, hitsPerPage, callback);
+  return exportData(this.searchSynonyms.bind(this), hitsPerPage, callback);
 };
 
 Index.prototype.saveSynonym = function(synonym, opts, callback) {
@@ -799,7 +800,7 @@ Index.prototype.searchRules = function(params, callback) {
  * @param [function] callback will be called after all query rules are retrieved
  */
 Index.prototype.exportRules = function(hitsPerPage, callback) {
-  return exportData(this.searchRules, hitsPerPage, callback);
+  return exportData(this.searchRules.bind(this), hitsPerPage, callback);
 };
 
 Index.prototype.saveRule = function(rule, opts, callback) {
