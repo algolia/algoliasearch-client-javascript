34f5c6b8886b124344ae3d801148dcae45891d13
Haroen Viaene <hello@haroen.me>
2018-03-21 16:53:21 +0100

    chore(test): retry browsers twice

--- zuul.config.js
+++ zuul.config.js
@@ -23,7 +23,7 @@ var zuulConfig = module.exports = {
   browser_output_timeout: 60 * 3 * 1000,
   browser_open_timeout: 60 * 6 * 1000,
   // we want to be notified something is wrong asap, so no retry
-  browser_retries: 1
+  browser_retries: 2
 };
 
 if (process.env.CI === 'true') {
