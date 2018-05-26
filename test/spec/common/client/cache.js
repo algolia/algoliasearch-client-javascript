2e9daf1d6bb28f978ed57eea1191db29dea1815c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-26 16:36:32 +0200

    feat(cache): enable test with original content

--- test/spec/common/client/cache.js
+++ test/spec/common/client/cache.js
@@ -44,44 +44,41 @@ test('expect to cache the requests', function(t) {
   });
 });
 
-// @TODO: enable this test when we are able to mock `responseText`
-// test('expect to always return the original response', function(t) {
-//   t.plan(2);
+test('expect to always return the original response', function(t) {
+  t.plan(2);
 
-//   var fauxJax = require('faux-jax');
-//   var createFixture = require('../../../utils/create-fixture');
-//   var fixture = createFixture();
-//   var credentials = fixture.credentials;
-//   var client = fixture.client;
+  var fauxJax = require('faux-jax');
+  var createFixture = require('../../../utils/create-fixture');
+  var fixture = createFixture();
+  var credentials = fixture.credentials;
+  var client = fixture.client;
 
-//   client._useCache = true;
+  client._useCache = true;
 
-//   fauxJax.install({gzip: true});
+  fauxJax.install({gzip: true});
 
-//   var count = 0;
-//   fauxJax.on('request', function(req) {
-//     count++;
-//     req.respond(200, {}, '{"hits": []}');
-//   });
+  fauxJax.on('request', function(req) {
+    req.respond(200, {}, '{"hits": []}');
+  });
 
-//   function search() {
-//     return client.search([
-//       {
-//         indexName: credentials.indexName,
-//         query: 'Hello'
-//       }
-//     ]);
-//   }
+  function search() {
+    return client.search([
+      {
+        indexName: credentials.indexName,
+        query: 'Hello'
+      }
+    ]);
+  }
 
-//   search().then(function(content) {
-//     t.deepEqual(content, {hits: []});
+  search().then(function(content) {
+    t.deepEqual(content, {hits: []});
 
-//     content.__shouldNotBePresentOnNextResolution = true;
+    content.__shouldNotBePresentOnNextResolution = true;
 
-//     return search();
-//   }).then(function(content) {
-//     t.deepEqual(content, {hits: []});
+    return search();
+  }).then(function(content) {
+    t.deepEqual(content, {hits: []});
 
-//     fauxJax.restore();
-//   });
-// });
+    fauxJax.restore();
+  });
+});
