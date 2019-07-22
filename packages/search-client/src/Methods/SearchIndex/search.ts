9f204c1dfcfc6d52d78d660e953640f875baffce
Nuno Maduro <enunomaduro@gmail.com>
2019-07-22 14:37:01 -0400

    feat(search): adds search method

--- packages/search-client/src/Methods/SearchIndex/search.ts
+++ packages/search-client/src/Methods/SearchIndex/search.ts
@@ -6,14 +6,16 @@ import { ConstructorOf } from '../../helpers';
 export const search = <TSearchIndex extends ConstructorOf<SearchIndex>>(base: TSearchIndex) => {
   return class extends base implements HasSearch {
     public search<THit>(
-      request: SearchRequest,
-      requestOptions?: RequestOptions
+      query: string,
+      requestOptions?: RequestOptions & SearchOptions
     ): Promise<SearchResponse<THit>> {
       return this.transporter.read(
         {
           method: Method.Post,
           path: `1/indexes/${this.indexName}/query`,
-          data: request,
+          data: {
+            query,
+          },
         },
         requestOptions
       );
@@ -23,15 +25,75 @@ export const search = <TSearchIndex extends ConstructorOf<SearchIndex>>(base: TS
 
 export interface HasSearch extends SearchIndex {
   search<THit>(
-    request: SearchRequest,
-    requestOptions?: RequestOptions
+    query: string,
+    requestOptions?: RequestOptions & SearchOptions
   ): Promise<SearchResponse<THit>>;
 }
 
-export type SearchRequest = {
-  query: string;
+export type SearchOptions = {
+  similarQuery?: string;
+  facetFilters?: string | Array<string | string[]>;
+  optionalFilters?: string | Array<string | string[]>;
+  numericFilters?: string | Array<string | string[]>;
+  tagFilters?: string | Array<string | string[]>;
+  sumOrFiltersScores?: boolean;
+  filters?: string;
+  page?: number;
+  hitsPerPage?: number;
+  offset?: number;
+  length?: number;
+  attributesToHighlight?: string[];
+  attributesToSnippet?: string[];
+  attributesToRetrieve?: string[];
+  highlightPreTag?: string;
+  highlightPostTag?: string;
+  snippetEllipsisText?: string;
+  restrictHighlightAndSnippetArrays?: boolean;
+  facets?: string | string[];
+  maxValuesPerFacet?: number;
+  facetingAfterDistinct?: boolean;
+  minWordSizefor1Typo?: number;
+  minWordSizefor2Typos?: number;
+  allowTyposOnNumericTokens?: boolean;
+  disableTypoToleranceOnAttributes?: string[];
+  queryType?: string;
+  removeWordsIfNoResults?: string;
+  advancedSyntax?: boolean;
+  advancedSyntaxFeatures?: string[];
+  optionalWords?: string[];
+  disableExactOnAttributes?: string[];
+  exactOnSingleWordQuery?: string;
+  alternativesAsExact?: string[];
+  enableRules?: boolean;
+  ruleContexts?: string[];
+  distinct?: number;
+  analytics?: boolean;
+  analyticsTags?: string[];
+  synonyms?: boolean;
+  replaceSynonymsInHighlight?: boolean;
+  minProximity?: number;
+  responseFields?: string[];
+  maxFacetHits?: number;
+  percentileComputation?: boolean;
+  clickAnalytics?: boolean;
+  personalizationImpact?: number;
+  enablePersonalization?: boolean;
+  restrictSearchableAttributes?: string[];
+  sortFacetValuesBy?: string;
+  typoTolerance?: any;
+  aroundLatLng?: string;
+  aroundLatLngViaIP?: boolean;
+  aroundRadius?: any;
+  aroundPrecision?: number;
+  minimumAroundRadius?: number;
+  insideBoundingBox?: number[][];
+  insidePolygon?: number[][];
+  ignorePlurals?: any;
+  removeStopWords?: string[];
+  getRankingInfo?: boolean;
 };
 
 export type SearchResponse<THit> = {
   hits: THit[];
+  queryID?: string;
 };
