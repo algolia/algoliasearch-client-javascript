1f98e909d9076b29f65fc0ea6c4f4b2a2535fdf4
Nuno Maduro <enunomaduro@gmail.com>
2019-09-17 09:45:03 +0200

    chore: refactor sauceOptions in saucelabs config

--- wdio.saucelabs.conf.js
+++ wdio.saucelabs.conf.js
@@ -4,6 +4,14 @@ require('dotenv').config();
 
 const baseConfig = require('./wdio.base.conf');
 
+const sauceOptions = {
+  // Force Selenium version on Firefox, solves an issue with `setValue`
+  // https://github.com/webdriverio/webdriverio/issues/3443
+  seleniumVersion: '3.11.0',
+  recordVideo: false,
+  recordScreenshots: false,
+};
+
 exports.config = {
   ...baseConfig,
   services: [...(baseConfig.services || []), 'sauce'],
@@ -16,19 +24,17 @@ exports.config = {
     {
       browserName: 'chrome',
       browserVersion: '76.0',
+      'sauce:options': sauceOptions,
     },
     {
       browserName: 'firefox',
       browserVersion: '68.0',
-      'sauce:options': {
-        // Force Selenium version on Firefox, solves an issue with `setValue`
-        // https://github.com/webdriverio/webdriverio/issues/3443
-        seleniumVersion: '3.11.0',
-      },
+      'sauce:options': sauceOptions,
     },
     {
       browserName: 'internet explorer',
       browserVersion: '11.285',
+      'sauce:options': sauceOptions,
     },
   ],
 };
