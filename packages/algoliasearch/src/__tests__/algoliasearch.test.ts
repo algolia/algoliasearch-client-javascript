1251441b83497aa1be18038508154add78cefe76
Nuno Maduro <enunomaduro@gmail.com>
2019-10-07 15:48:10 +0200

    chore: fixes algolioasearch test

--- packages/algoliasearch/src/__tests__/algoliasearch.test.ts
+++ packages/algoliasearch/src/__tests__/algoliasearch.test.ts
@@ -22,12 +22,12 @@ describe('algoliasearch', () => {
 
     expect(client.transporter.headers).toEqual({
       'content-type': 'application/x-www-form-urlencoded',
+      'x-algolia-application-id': 'foo',
+      'x-algolia-api-key': 'bar',
     });
 
     expect(client.transporter.queryParameters).toEqual({
       'x-algolia-agent': 'Algolia for JavaScript (4.0.0-alpha.0); Browser',
-      'x-algolia-application-id': 'foo',
-      'x-algolia-api-key': 'bar',
     });
   });
 });
