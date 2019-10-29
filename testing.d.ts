83e9d121aa8b0d00e9085c3ca1d9cf78fd5068f5
Nuno Maduro <enunomaduro@gmail.com>
2019-10-29 20:47:28 +0100

    tests: removes unused helpers

--- testing.d.ts
+++ testing.d.ts
@@ -1,11 +1,4 @@
-import { Cache } from '@algolia/cache-common';
-import { Requester } from '@algolia/requester-common/src/types/Requester';
-
 declare const testing: {
-  readonly requester: () => Requester;
-  readonly requestsCache: () => Cache;
-  readonly hostsCache: () => Cache;
-  readonly responsesCache: () => Cache;
   readonly environment: () => string;
   readonly isBrowser: () => boolean;
 };
