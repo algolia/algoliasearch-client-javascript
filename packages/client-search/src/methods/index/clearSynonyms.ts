85577f8166d59726fb14c103cc30b305bb29fcb7
Nuno Maduro <enunomaduro@gmail.com>
2019-11-04 22:03:21 +0100

    fix: ClearSynonymsOptions file name

--- packages/client-search/src/methods/index/clearSynonyms.ts
+++ packages/client-search/src/methods/index/clearSynonyms.ts
@@ -4,7 +4,7 @@ import { WaitablePromise } from '@algolia/client-common/types/WaitablePromise';
 import { MethodEnum } from '@algolia/requester-common/types/MethodType';
 import { RequestOptions } from '@algolia/transporter/types/RequestOptions';
 
-import { ClearSynonymsOptions } from '../../types/clearSynonymsOptions';
+import { ClearSynonymsOptions } from '../../types/ClearSynonymsOptions';
 import { DeleteResponse } from '../../types/DeleteResponse';
 import { SearchIndex } from '../../types/SearchIndex';
 import { HasWaitTask, waitTask } from './waitTask';
