c0e2b88a2e999a9a98c514405ee0e46d2cbd7752
Nuno Maduro <enunomaduro@gmail.com>
2019-11-06 15:36:05 +0100

    chore: fix playground starter

--- scripts/prepare-playground.sh
+++ scripts/prepare-playground.sh
@@ -1,2 +1,2 @@
-cp -a packages/algoliasearch/dist/algoliasearch-lite.umd.min* playground/browser-lite/.
-cp -a packages/algoliasearch/dist/algoliasearch.umd.min* playground/browser/.
+cp -a packages/algoliasearch/dist/algoliasearch-lite.umd* playground/browser-lite/.
+cp -a packages/algoliasearch/dist/algoliasearch.umd* playground/browser/.
