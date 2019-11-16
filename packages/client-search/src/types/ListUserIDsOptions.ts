745e4e205631f3fd171048351e856197246332f4
Nuno Maduro <enunomaduro@gmail.com>
2019-11-16 02:30:12 +0100

    feat(mcm): adds list user ids type

---
+++ packages/client-search/src/types/ListUserIDsOptions.ts
@@ -0,0 +1,11 @@
+export type ListUserIDsOptions = {
+  /**
+   * Page to fetch.
+   */
+  readonly page?: number;
+
+  /**
+   * Number of users to retrieve per page.
+   */
+  readonly hitsPerPage?: number;
+};
