67bf277e7890073f1e449886525557cd168bbfee
Nuno Maduro <enunomaduro@gmail.com>
2019-11-22 20:22:23 +0100

    refacto: params extractor in multiple search for facet values

--- packages/client-search/src/methods/client/multipleSearchForFacetValues.ts
+++ packages/client-search/src/methods/client/multipleSearchForFacetValues.ts
@@ -19,16 +19,7 @@ export const multipleSearchForFacetValues = (base: SearchClient) => {
   ): Readonly<Promise<readonly SearchForFacetValuesResponse[]>> => {
     return Promise.all(
       queries.map(query => {
-        const params = Object.assign({}, query.params);
-        const facetName = params.facetName;
-        const facetQuery = params.facetQuery;
-
-        // @ts-ignore
-        // eslint-disable-next-line functional/immutable-data, no-param-reassign
-        delete params.facetName;
-        // @ts-ignore
-        // eslint-disable-next-line functional/immutable-data, no-param-reassign
-        delete params.facetQuery;
+        const { facetName, facetQuery, ...params } = query.params;
 
         return initIndex(base)(query.indexName, {
           methods: { searchForFacetValues },
