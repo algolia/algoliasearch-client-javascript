9c17029a6c0b84aecca1cffd43b9da1ec322bd9c
Nuno Maduro <enunomaduro@gmail.com>
2019-12-13 01:05:19 +0100

    tests: removes temporary code for tests

--- packages/client-common/src/__tests__/TestSuite.ts
+++ packages/client-common/src/__tests__/TestSuite.ts
@@ -20,16 +20,8 @@ export class TestSuite {
   public async cleanUp(): Promise<void> {
     for (const index of this.indices) {
       // Indices must by deleted sequencially and waiting..
-      try {
-        await index.delete().wait();
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(index.indexName);
 
-        // eslint-disable-next-line no-console
-        console.log(error);
-        await index.delete().wait();
-      }
+      await index.delete().wait();
     }
   }
 
