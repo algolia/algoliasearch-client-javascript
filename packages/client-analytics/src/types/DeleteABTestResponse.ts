774e92ca103025f456b888ec8a3a46088db0f2a2
Nuno Maduro <enunomaduro@gmail.com>
2020-01-07 11:50:07 +0100

    fix: adds missing field in ab tests responses

--- packages/client-analytics/src/types/DeleteABTestResponse.ts
+++ packages/client-analytics/src/types/DeleteABTestResponse.ts
@@ -1,5 +1,10 @@
 export type DeleteABTestResponse = {
   /**
+   * The ab test unique identifier.
+   */
+  readonly abTestID: number;
+
+  /**
    * The operation task id. May be used to perform a wait task.
    */
   readonly taskID: number;
