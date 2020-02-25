f826fb5d46cfdd412d73f635aa78c71a94e859b3
Nuno Maduro <enunomaduro@gmail.com>
2020-02-25 14:15:43 +0100

    feat(explain-in-search-response): adds type

--- packages/client-search/src/types/SearchResponse.ts
+++ packages/client-search/src/types/SearchResponse.ts
@@ -38,4 +38,50 @@ export type SearchResponse<TObject = {}> = {
   readonly parsedQuery?: string;
   readonly userData?: any;
   readonly appliedRules?: ReadonlyArray<Readonly<Record<string, any>>>;
+
+  /**
+   * The explanation of the decompounding at query time.
+   */
+  readonly explain?: {
+    /**
+     * The explain query match.
+     */
+    readonly match: {
+      /**
+       * The explain query match alternatives.
+       */
+      readonly alternatives: ReadonlyArray<{
+        /**
+         * The alternative type.
+         */
+        readonly types: readonly string[];
+
+        /**
+         * The list of alternative words.
+         */
+        readonly words: readonly string[];
+
+        /**
+         * The number of typos.
+         */
+        readonly typos: number;
+
+        /**
+         * The offset.
+         */
+        readonly offset: number;
+
+        /**
+         * The length.
+         */
+        readonly length: number;
+      }>;
+    };
+
+    /**
+     * Query parameter reporting. Parameters are reported
+     * as a JSON object with one field per parameter.
+     */
+    readonly params?: Readonly<Record<string, any>>;
+  };
 };
