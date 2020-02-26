4e3d61e58f3ba25e257a254b67d12c8708328bd9
Nuno Maduro <enunomaduro@gmail.com>
2020-02-26 10:03:13 +0100

    feat: adds explain in search response (#1036)
    * feat(explain-in-search-response): adds type
    
    * feat(explain-in-search-response): adds tests

--- packages/client-search/src/__tests__/integration/search.test.ts
+++ packages/client-search/src/__tests__/integration/search.test.ts
@@ -1,4 +1,4 @@
-import { createObjectNotFoundError } from '../..';
+import { createObjectNotFoundError, SearchResponse } from '../..';
 import { waitResponses } from '../../../../client-common/src/__tests__/helpers';
 import { TestSuite } from '../../../../client-common/src/__tests__/TestSuite';
 
@@ -132,4 +132,134 @@ test(testSuite.testName, async () => {
       value: 'Arista Networks',
     },
   ]);
+});
+
+test('decompounding at query time', async () => {
+  const index = testSuite.makeIndex();
+
+  await waitResponses([
+    index.setSettings({
+      decompoundedAttributes: {
+        de: ['category', 'type', 'desc'],
+      },
+      queryLanguages: ['de'],
+      ignorePlurals: true,
+    }),
+    index.saveObjects([
+      { objectID: 'A...B', category: 'hunde', type: 'Hütte' },
+      { objectID: 'AB', desc: 'Ich will ein schöne Hundehütte' },
+      { objectID: 'BA', desc: 'Hüttehunde, Es ist eine lustige Verbindung!' },
+      { objectID: 'B...A', category: 'Eine Hütte für Hunden' },
+      { objectID: 'A', category: 'Hunde' },
+      { objectID: 'B', category: 'Hunde' },
+    ]),
+  ]);
+
+  const results = await index.search('Hundehütte', {
+    explain: ['match.alternatives'],
+    ignorePlurals: true,
+    queryParameters: {
+      foo: 'bar',
+    },
+  });
+
+  const explain: SearchResponse['explain'] = {
+    match: {
+      alternatives: [
+        {
+          types: ['original'],
+          words: ['hunde'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['original'],
+          words: ['hutte'],
+          typos: 0,
+          offset: 1,
+          length: 1,
+        },
+        {
+          types: ['compound'],
+          words: ['hundehutte'],
+          typos: 0,
+          offset: 0,
+          length: 2,
+        },
+        {
+          types: ['split'],
+          words: ['hunde', 'hutte'],
+          typos: 0,
+          offset: 0,
+          length: 2,
+        },
+        {
+          types: ['plural'],
+          words: ['hund'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hundchen'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hundchens'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hundehutten'],
+          typos: 0,
+          offset: 0,
+          length: 2,
+        },
+        {
+          types: ['plural'],
+          words: ['hunden'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hundes'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hundlein'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hunds'],
+          typos: 0,
+          offset: 0,
+          length: 1,
+        },
+        {
+          types: ['plural'],
+          words: ['hutten'],
+          typos: 0,
+          offset: 1,
+          length: 1,
+        },
+      ],
+    },
+  };
+
+  expect(results.explain).toMatchObject(explain);
 });
