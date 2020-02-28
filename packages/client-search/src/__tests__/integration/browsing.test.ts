f83a911c66354b7ea551f6bb2e0dce951a841212
Haroen Viaene <hello@haroen.me>
2020-02-28 14:58:35 +0100

    fix(browse): expose original error from browse request (#1042)

--- packages/client-search/src/__tests__/integration/browsing.test.ts
+++ packages/client-search/src/__tests__/integration/browsing.test.ts
@@ -11,8 +11,17 @@ import { TestSuite } from '../../../../client-common/src/__tests__/TestSuite';
 
 const testSuite = new TestSuite('browsing');
 
-test(testSuite.testName, async () => {
-  const index = testSuite.makeIndex();
+let index: ReturnType<typeof testSuite.makeIndex>;
+let browse: (
+  method: (index: SearchIndex) => any,
+  options?: { hitsPerPage?: number }
+) => Promise<{
+  hitsCount: number;
+  batchCount: number;
+}>;
+
+beforeAll(async () => {
+  index = testSuite.makeIndex();
 
   // First lets create the index.
   await index.setSettings({}).wait();
@@ -20,10 +29,7 @@ test(testSuite.testName, async () => {
   // Then, let's create a generic browse function that
   // will take a browseMethod, and get metrics such as
   // the number of retrieved hits and number of batches.
-  const browse = async (
-    method: (index: SearchIndex) => any,
-    options: { hitsPerPage?: number } = {}
-  ) => {
+  browse = async (method, options = {}) => {
     const result = {
       hitsCount: 0,
       batchCount: 0,
@@ -41,102 +47,120 @@ test(testSuite.testName, async () => {
 
     return result;
   };
+});
 
-  // Next, let's create a test case for each browse type, note that
-  // each test case as a specific method, a specific way of browsing,
-  // and also a specific way of reseting the dataset.
+describe.each([
+  [
+    'browseObjects',
+    {
+      method: browseObjects,
+      async withDataset(number: number) {
+        await index.clearObjects();
 
-  const objectsCase = {
-    method: browseObjects,
-    async withDataset(number: number) {
-      await index.clearObjects();
-
-      await index.saveObjects(createFaker().objects(number)).wait();
+        await index.saveObjects(createFaker().objects(number)).wait();
+      },
+      cursorBased: true,
     },
-    cursorBased: true,
-  };
+  ],
+  [
+    'browseRules',
+    {
+      method: browseRules,
+      async withDataset(number: number) {
+        await index.clearRules();
 
-  const rulesCase = {
-    method: browseRules,
-    async withDataset(number: number) {
-      await index.clearRules();
-
-      await index
-        .saveRules(
-          [...Array(number).keys()].map(objectID => ({
-            objectID: objectID.toString(),
-            consequence: { params: { filters: 'brand:OnePlus' } },
-          }))
-        )
-        .wait();
+        await index
+          .saveRules(
+            [...Array(number).keys()].map(objectID => ({
+              objectID: objectID.toString(),
+              consequence: { params: { filters: 'brand:OnePlus' } },
+            }))
+          )
+          .wait();
+      },
+      cursorBased: false,
     },
-    cursorBased: false,
-  };
+  ],
+  [
+    'browseSynonyms',
+    {
+      method: browseSynonyms,
+      async withDataset(number: number) {
+        await index.clearSynonyms();
 
-  const synonymsCase = {
-    method: browseSynonyms,
-    async withDataset(number: number) {
-      await index.clearSynonyms();
-
-      await index
-        .saveSynonyms(
-          [...Array(number).keys()].map(objectID => ({
-            objectID: objectID.toString(),
-            type: 'synonym',
-            synonyms: ['gba', 'gameboy advance', 'game boy advance'],
-          }))
-        )
-        .wait();
+        await index
+          .saveSynonyms(
+            [...Array(number).keys()].map(objectID => ({
+              objectID: objectID.toString(),
+              type: 'synonym',
+              synonyms: ['gba', 'gameboy advance', 'game boy advance'],
+            }))
+          )
+          .wait();
+      },
+      cursorBased: false,
     },
-    cursorBased: false,
-  };
+  ],
+])(`${testSuite.testName}: %s`, (_methodName, testCase) => {
+  test('empty', async () => {
+    await expect(browse(testCase.method)).resolves.toEqual({
+      hitsCount: 0,
+      batchCount: 1,
+    });
+  });
 
-  // Finaly let's start the testing part. For each browse type lets test
-  // it against, and multiple numbers of datasets and conditions.
-  await Promise.all(
-    [objectsCase, synonymsCase, rulesCase].map(async testCase => {
-      await expect(browse(testCase.method)).resolves.toEqual({
-        hitsCount: 0,
+  test('dataset 10', async () => {
+    await testCase.withDataset(10);
+
+    await Promise.all([
+      expect(browse(testCase.method)).resolves.toEqual({
+        hitsCount: 10,
         batchCount: 1,
-      });
+      }),
 
-      await testCase.withDataset(10);
+      expect(browse(testCase.method, { hitsPerPage: 9 })).resolves.toEqual({
+        hitsCount: 10,
+        batchCount: 2,
+      }),
 
-      await Promise.all([
-        expect(browse(testCase.method)).resolves.toEqual({
-          hitsCount: 10,
-          batchCount: 1,
-        }),
+      expect(browse(testCase.method, { hitsPerPage: 10 })).resolves.toEqual({
+        hitsCount: 10,
+        batchCount: testCase.cursorBased ? 1 : 2,
+      }),
 
-        expect(browse(testCase.method, { hitsPerPage: 9 })).resolves.toEqual({
-          hitsCount: 10,
-          batchCount: 2,
-        }),
+      expect(browse(testCase.method, { hitsPerPage: 11 })).resolves.toEqual({
+        hitsCount: 10,
+        batchCount: 1,
+      }),
+    ]);
+  });
 
-        expect(browse(testCase.method, { hitsPerPage: 10 })).resolves.toEqual({
-          hitsCount: 10,
-          batchCount: testCase.cursorBased ? 1 : 2,
-        }),
+  test('dataset 1000', async () => {
+    await testCase.withDataset(1000);
 
-        expect(browse(testCase.method, { hitsPerPage: 11 })).resolves.toEqual({
-          hitsCount: 10,
-          batchCount: 1,
-        }),
-      ]);
+    await Promise.all([
+      expect(browse(testCase.method)).resolves.toEqual({
+        hitsCount: 1000,
+        batchCount: testCase.cursorBased ? 1 : 2,
+      }),
 
-      await testCase.withDataset(1000);
+      expect(browse(testCase.method, { hitsPerPage: 999 })).resolves.toEqual({
+        hitsCount: 1000,
+        batchCount: 2,
+      }),
+    ]);
+  });
 
-      await Promise.all([
-        expect(browse(testCase.method)).resolves.toEqual({
-          hitsCount: 1000,
-          batchCount: testCase.cursorBased ? 1 : 2,
-        }),
-
-        expect(browse(testCase.method, { hitsPerPage: 999 })).resolves.toEqual({
-          hitsCount: 1000,
-          batchCount: 2,
-        }),
-      ]);
-    })
-  );
+  test('error inside browse method', async () => {
+    await expect(
+      testCase.method(index)({
+        unknownParameter: 'fail',
+      })
+    ).rejects.toEqual(
+      expect.objectContaining({
+        message: expect.any(String),
+        name: 'ApiError',
+      })
+    );
+  });
 });
--- scripts/prepare-test-unit.sh
+++ scripts/prepare-test-unit.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 kill -9 `ps aux | grep test-server-for-timeouts | awk '{print $2}'` 2> /dev/null || : && node scripts/test-server-for-timeouts.js &
