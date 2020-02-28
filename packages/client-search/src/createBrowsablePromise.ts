3e7c5598a4ab0d4dd8cf3f38f43125354e977c14
Haroen Viaene <hello@haroen.me>
2020-02-28 13:30:01 +0100

    fix(browse): expose original error from browse request

--- packages/client-search/src/createBrowsablePromise.ts
+++ packages/client-search/src/createBrowsablePromise.ts
@@ -6,43 +6,41 @@ export function createBrowsablePromise<TObject>(
     readonly request: (data: BrowseRequestData) => Readonly<Promise<BrowseResponse<TObject>>>;
   } & BrowseOptions<TObject>
 ): Readonly<Promise<void>> {
-  return new Promise<void>(resolve => {
-    const browse = (data: BrowseRequestData = {}): Promise<void> => {
-      return options.request(data).then(response => {
-        /**
-         * First we send to the developer the
-         * batch retrieved from the API.
-         */
-        if (options.batch !== undefined) {
-          options.batch(response.hits);
-        }
+  const browse = (data: BrowseRequestData): Promise<void> => {
+    return options.request(data).then(response => {
+      /**
+       * First we send to the developer the
+       * batch retrieved from the API.
+       */
+      if (options.batch !== undefined) {
+        options.batch(response.hits);
+      }
 
-        /**
-         * Then, we ask to the browse concrete implementation
-         * if we should stop browsing. As example, the `browseObjects`
-         * method will stop if the cursor is not present on the response.
-         */
-        if (options.shouldStop(response)) {
-          return resolve();
-        }
+      /**
+       * Then, we ask to the browse concrete implementation
+       * if we should stop browsing. As example, the `browseObjects`
+       * method will stop if the cursor is not present on the response.
+       */
+      if (options.shouldStop(response)) {
+        return undefined;
+      }
 
-        /**
-         * Finally, if the response contains a cursor, we browse to the next
-         * batch using that same cursor. Otherwise, we just use the traditional
-         * browsing using the page element.
-         */
-        if (response.cursor) {
-          return browse({
-            cursor: response.cursor,
-          });
-        }
-
+      /**
+       * Finally, if the response contains a cursor, we browse to the next
+       * batch using that same cursor. Otherwise, we just use the traditional
+       * browsing using the page element.
+       */
+      if (response.cursor) {
         return browse({
-          page: (data.page || 0) + 1,
+          cursor: response.cursor,
         });
-      });
-    };
+      }
 
-    return browse();
-  });
+      return browse({
+        page: (data.page || 0) + 1,
+      });
+    });
+  };
+
+  return browse({});
 }
