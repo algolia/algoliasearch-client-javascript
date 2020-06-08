2fb6a4d82bb52121c9260e5c5a00e704bb4e914d
Nuno Maduro <enunomaduro@gmail.com>
2020-06-08 14:16:54 +0200

    feat(browse): updates comments on type

--- packages/client-search/src/types/BrowseOptions.ts
+++ packages/client-search/src/types/BrowseOptions.ts
@@ -6,8 +6,10 @@ export type BrowseOptions<TObject> = {
    * The callback called for each batch of objects.
    */
   readonly batch?: (batch: ReadonlyArray<TObject & ObjectWithObjectID>) => any;
+
   /**
-   * condition to stop browsing. By default this checks whether there's any more content to get
+   * The callback called to determine if the browse should stop. By
+   * default this checks whether there's any more content to get.
    */
   readonly shouldStop?: (response: BrowseResponse<TObject>) => boolean;
 };
