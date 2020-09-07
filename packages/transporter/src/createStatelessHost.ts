1af5588958f719e461b40a9093e30b2dce6258c3
Haroen Viaene <hello@haroen.me>
2020-09-07 11:32:49 +0200

    feat(hosts): accept url as string
    In v3 the API was `hosts: string[]`. It's easy to miss out on this in the migration guide, and it's also simpler to write just a string in the general case
    
    fixes #1188

--- packages/transporter/src/createStatelessHost.ts
+++ packages/transporter/src/createStatelessHost.ts
@@ -1,6 +1,14 @@
 import { CallEnum, HostOptions, StatelessHost } from '.';
 
 export function createStatelessHost(options: HostOptions): StatelessHost {
+  if (typeof options === 'string') {
+    return {
+      protocol: 'https',
+      url: options,
+      accept: CallEnum.Any,
+    };
+  }
+
   return {
     protocol: options.protocol || 'https',
     url: options.url,
