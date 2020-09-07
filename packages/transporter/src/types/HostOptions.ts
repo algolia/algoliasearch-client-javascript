4daf32c4bad83067b56e72f1f3da6e603528ff55
Haroen Viaene <hello@haroen.me>
2020-09-07 15:41:40 +0200

    feat(hosts): accept url as string (#1189)
    * feat(hosts): accept url as string
    
    In v3 the API was `hosts: string[]`. It's easy to miss out on this in the migration guide, and it's also simpler to write just a string in the general case
    
    fixes #1188
    
    * chore(bundlesize): loosen

--- packages/transporter/src/types/HostOptions.ts
+++ packages/transporter/src/types/HostOptions.ts
@@ -1,18 +1,20 @@
 import { CallType } from '.';
 
-export type HostOptions = {
-  /**
-   * The url of the server, without the protocol.
-   */
-  readonly url: string;
+export type HostOptions =
+  | string
+  | {
+      /**
+       * The url of the server, without the protocol.
+       */
+      readonly url: string;
 
-  /**
-   * The type of host. Defaults to `Any`.
-   */
-  readonly accept?: CallType;
+      /**
+       * The type of host. Defaults to `Any`.
+       */
+      readonly accept?: CallType;
 
-  /**
-   * The protocol. Defaults to `https`.
-   */
-  readonly protocol?: string;
-};
+      /**
+       * The protocol. Defaults to `https`.
+       */
+      readonly protocol?: string;
+    };
