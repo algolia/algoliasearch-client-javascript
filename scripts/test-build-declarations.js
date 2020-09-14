5bf4bd2dcc9063700f14c9aaca5b3bd021374ee7
Haroen Viaene <hello@haroen.me>
2020-09-14 15:30:43 +0200

    test(declarations): better error message

--- scripts/test-build-declarations.js
+++ scripts/test-build-declarations.js
@@ -16,7 +16,12 @@ const NUMBER_OF_DECLARATIONS = 18;
   });
 
   if (declarations.length !== NUMBER_OF_DECLARATIONS) {
-    console.log('Unexpected number of declarations');
+    console.log(
+      'Unexpected number of declarations. expected:',
+      NUMBER_OF_DECLARATIONS,
+      'but got:',
+      declarations.length
+    );
     process.exit(1);
   }
 
