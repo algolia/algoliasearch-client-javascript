33d861b21f07644b2345dccaab81fff3d60df622
Anthony Seure <anthony.seure@gmail.com>
2020-09-22 10:26:24 +0200

    chore: migrate release process to Ship.js
    This commit introduces a new release process by:
    
    - removing the need for the scripts/release.sh bash script in favor of
      https://github.com/algolia/shipjs.
    - automating the release in CircleCI
    
    Notable changes are:
    
    - `version` attribute added to the `package.json` file: this is a bit
      redundant with the `version` attribute available in the `lerna.json`
      but it is required by `conventional-changelog-cli`, which is used by Ship.js
      to generate new Changelog entries, and can only identify versions based
      on the `package.json` file.
    - `CHANGELOG.md` text header has been removed because
      `conventional-changelog-ci` is not aware of headers in Changelog
      files.
    - `GITHUB_TOKEN` environment variable is now added to the CircleCI
      configuration. It grants CircleCI with the appropriate rights to run
      `shipjs trigger`, effectively publishing to NPM. The value is set to
      the Github account `algolia-api-client-bot` which can be retrieved
      from our Vault cluster, like other credentials.
    
    New proposed release process is composed of the following steps:
    
    ```
    GITHUB_TOKEN=XXX yarn release
    
    git checkout master
    git pull --rebase
    git tag X.Y.Z
    git push && git push --tags
    
    ```

---
+++ conventional-changelog.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable functional/immutable-data */
+/* eslint-disable import/no-commonjs */
+/* eslint-disable max-len */
+
+module.exports = {
+  writerOpts: {
+    headerPartial:
+      '## [v{{version}}](https://github.com/algolia/algoliasearch-client-javascript/compare/{{previousTag}}...{{version}})\n',
+  },
+};
