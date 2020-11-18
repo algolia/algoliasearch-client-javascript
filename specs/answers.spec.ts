83906d7e673c30453e1b34502281e040361d5327
eunjae-lee <karis612@gmail.com>
2020-11-18 15:04:55 +0100

    test: add browser-lite to integration test for answers

--- specs/answers.spec.ts
+++ specs/answers.spec.ts
@@ -18,53 +18,55 @@ const credentials = {
   // apiKey: `${process.env.ALGOLIA_SEARCH_KEY_1}`
 };
 
-describe("answers features - algoliasearch.com", () => {
-  beforeEach(async () => browser.url("algoliasearch.com"));
+["algoliasearch-lite.com", "algoliasearch.com"].forEach(preset => {
+  describe(`answers features - ${preset}`, () => {
+    beforeEach(async () => browser.url(preset));
 
-  it("searchIndex::findAnswers", async () => {
-    const results: any = await browser.executeAsync(function(
-      credentials,
-      done
-    ) {
-      const client = algoliasearch(credentials.appId, credentials.apiKey);
+    it("searchIndex::findAnswers", async () => {
+      const results: any = await browser.executeAsync(function(
+        credentials,
+        done
+      ) {
+        const client = algoliasearch(credentials.appId, credentials.apiKey);
 
-      // TODO: remove this customization once the engine accepts url encoded query params
-      client.transporter.userAgent.value = "answers-test";
+        // TODO: remove this customization once the engine accepts url encoded query params
+        client.transporter.userAgent.value = "answers-test";
 
-      const index = client.initIndex("ted");
+        const index = client.initIndex("ted");
 
-      Promise.all([
-        index.findAnswers("sir ken robinson", ["en"]),
-        index.findAnswers("what", ["en"]),
-        index.findAnswers("sarah jones", ["en"], {
-          nbHits: 2,
-          attributesForPrediction: ["main_speaker"],
-          params: {
-            highlightPreTag: "_pre_",
-            highlightPostTag: "_post_"
-          }
-        })
-      ]).then(function(responses) {
-        done({
-          kenRobinson: responses[0],
-          what: responses[1],
-          sarah: responses[2]
+        Promise.all([
+          index.findAnswers("sir ken robinson", ["en"]),
+          index.findAnswers("what", ["en"]),
+          index.findAnswers("sarah jones", ["en"], {
+            nbHits: 2,
+            attributesForPrediction: ["main_speaker"],
+            params: {
+              highlightPreTag: "_pre_",
+              highlightPostTag: "_post_"
+            }
+          })
+        ]).then(function(responses) {
+          done({
+            kenRobinson: responses[0],
+            what: responses[1],
+            sarah: responses[2]
+          });
         });
-      });
-    },
-    credentials);
+      },
+      credentials);
 
-    expect(results.kenRobinson.nbHits).toBe(10);
+      expect(results.kenRobinson.nbHits).toBe(10);
 
-    expect(results.what.nbHits).toBe(0);
+      expect(results.what.nbHits).toBe(0);
 
-    expect(results.sarah.nbHits).toBe(1);
-    expect(results.sarah.hits[0]._highlightResult.main_speaker.value).toBe(
-      "_pre_Sarah_post_ _pre_Jones_post_"
-    );
+      expect(results.sarah.nbHits).toBe(1);
+      expect(results.sarah.hits[0]._highlightResult.main_speaker.value).toBe(
+        "_pre_Sarah_post_ _pre_Jones_post_"
+      );
 
-    expect(results.sarah.hits[0]._answer.extract).toBe(
-      "_pre_Sarah_post_ _pre_Jones_post_"
-    );
+      expect(results.sarah.hits[0]._answer.extract).toBe(
+        "_pre_Sarah_post_ _pre_Jones_post_"
+      );
+    });
   });
 });
