3007cb4ca213e0f85fc87c3218ee1a9caf101768
Clément Vannicatte <vannicattec@gmail.com>
2021-08-25 15:13:27 +0200

    fix: test `threshold` value

--- packages/recommend/src/__tests__/getRecommendations.test.ts
+++ packages/recommend/src/__tests__/getRecommendations.test.ts
@@ -126,7 +126,7 @@ describe('getRecommendations', () => {
     );
   });
 
-  test('override `undefined` threshold with default value', async () => {
+  test('overrides `undefined` threshold with default value', async () => {
     const client = createMockedClient();
 
     await client.getRecommendations(
@@ -152,6 +152,42 @@ describe('getRecommendations', () => {
               model: 'bought-together',
               objectID: 'B018APC4LE',
               threshold: 0,
+            },
+          ],
+        },
+        method: 'POST',
+        path: '1/indexes/*/recommendations',
+      },
+      {}
+    );
+  });
+
+  test('overrides default threshold value', async () => {
+    const client = createMockedClient();
+
+    await client.getRecommendations(
+      [
+        {
+          model: 'bought-together',
+          indexName: 'products',
+          objectID: 'B018APC4LE',
+          threshold: 42,
+        },
+      ],
+      {}
+    );
+
+    expect(client.transporter.read).toHaveBeenCalledTimes(1);
+    expect(client.transporter.read).toHaveBeenCalledWith(
+      {
+        cacheable: true,
+        data: {
+          requests: [
+            {
+              indexName: 'products',
+              model: 'bought-together',
+              objectID: 'B018APC4LE',
+              threshold: 42,
             },
           ],
         },
