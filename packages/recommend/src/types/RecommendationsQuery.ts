b6ad4296b093eb00d2f5d0ef8ab278fbab0cd8db
François Chalifour <francoischalifour@users.noreply.github.com>
2021-09-13 11:26:14 +0200

    docs(recommend): add TSDoc

--- packages/recommend/src/types/RecommendationsQuery.ts
+++ packages/recommend/src/types/RecommendationsQuery.ts
@@ -2,11 +2,40 @@ import { RecommendModel } from './RecommendModel';
 import { RecommendSearchOptions } from './RecommendSearchOptions';
 
 export type RecommendationsQuery = {
+  /**
+   * The name of the target index.
+   */
   readonly indexName: string;
+
+  /**
+   * The name of the Recommendation model to use.
+   */
   readonly model: RecommendModel;
+
+  /**
+   * The `objectID` of the item to get recommendations for.
+   */
   readonly objectID: string;
+
+  /**
+   * Threshold for the recommendations confidence score (between 0 and 100). Only recommendations with a greater score are returned.
+   */
   readonly threshold?: number;
+
+  /**
+   * How many recommendations to retrieve.
+   */
   readonly maxRecommendations?: number;
+
+  /**
+   * List of [search parameters](https://www.algolia.com/doc/api-reference/search-api-parameters/) to send.
+   */
   readonly queryParameters?: RecommendSearchOptions;
+
+  /**
+   * List of [search parameters](https://www.algolia.com/doc/api-reference/search-api-parameters/) to send.
+   *
+   * Additional filters to use as fallback when there aren’t enough recommendations.
+   */
   readonly fallbackParameters?: RecommendSearchOptions;
 };
