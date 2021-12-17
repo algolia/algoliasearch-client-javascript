eab3283c0ff1bdcfe7ffc35ea0164114129d2948
Devin Beeuwkes <46448173+DevinCodes@users.noreply.github.com>
2021-12-17 15:19:23 +0100

    feat(spec/search): add vault endpoints to spec (APIC-196) (#35)
    Co-authored-by: Clément Vannicatte <vannicattec@gmail.com>

---
+++ clients/algoliasearch-client-java-2/algoliasearch-core/com/algolia/model/DeleteSourceResponse.java
@@ -0,0 +1,77 @@
+package com.algolia.model;
+
+import com.google.gson.annotations.SerializedName;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+/** DeleteSourceResponse */
+public class DeleteSourceResponse {
+
+  public static final String SERIALIZED_NAME_DELETED_AT = "deletedAt";
+
+  @SerializedName(SERIALIZED_NAME_DELETED_AT)
+  private OffsetDateTime deletedAt;
+
+  public DeleteSourceResponse deletedAt(OffsetDateTime deletedAt) {
+    this.deletedAt = deletedAt;
+    return this;
+  }
+
+  /**
+   * Date of deletion (ISO-8601 format).
+   *
+   * @return deletedAt
+   */
+  @javax.annotation.Nonnull
+  @ApiModelProperty(
+    required = true,
+    value = "Date of deletion (ISO-8601 format)."
+  )
+  public OffsetDateTime getDeletedAt() {
+    return deletedAt;
+  }
+
+  public void setDeletedAt(OffsetDateTime deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DeleteSourceResponse deleteSourceResponse = (DeleteSourceResponse) o;
+    return Objects.equals(this.deletedAt, deleteSourceResponse.deletedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(deletedAt);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class DeleteSourceResponse {\n");
+    sb
+      .append("    deletedAt: ")
+      .append(toIndentedString(deletedAt))
+      .append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ clients/algoliasearch-client-java-2/algoliasearch-core/com/algolia/model/ReplaceSourceResponse.java
@@ -0,0 +1,77 @@
+package com.algolia.model;
+
+import com.google.gson.annotations.SerializedName;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+/** ReplaceSourceResponse */
+public class ReplaceSourceResponse {
+
+  public static final String SERIALIZED_NAME_UPDATED_AT = "updatedAt";
+
+  @SerializedName(SERIALIZED_NAME_UPDATED_AT)
+  private OffsetDateTime updatedAt;
+
+  public ReplaceSourceResponse updatedAt(OffsetDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+    return this;
+  }
+
+  /**
+   * Date of last update (ISO-8601 format).
+   *
+   * @return updatedAt
+   */
+  @javax.annotation.Nonnull
+  @ApiModelProperty(
+    required = true,
+    value = "Date of last update (ISO-8601 format)."
+  )
+  public OffsetDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(OffsetDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ReplaceSourceResponse replaceSourceResponse = (ReplaceSourceResponse) o;
+    return Objects.equals(this.updatedAt, replaceSourceResponse.updatedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(updatedAt);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ReplaceSourceResponse {\n");
+    sb
+      .append("    updatedAt: ")
+      .append(toIndentedString(updatedAt))
+      .append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
