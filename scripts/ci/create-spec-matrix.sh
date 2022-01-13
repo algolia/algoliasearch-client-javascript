9c3348400f35c1746fea056e7d5240253f79821a
Pierre Millot <pierre.millot@algolia.com>
2022-01-13 09:51:04 +0100

    chore: use matrix for the CI APIC-255 (#75)

---
+++ scripts/ci/create-spec-matrix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASE_CHANGED=$1
+BASE_BRANCH=$2
+
+specs='{"client":[]}'
+generators=( $(cat openapitools.json | jq '."generator-cli".generators' | jq -r 'keys[]') )
+
+for generator in "${generators[@]}"; do
+    client=${generator#*-}
+    if [[ ! ${specs[*]} =~ $client ]]; then
+        changed=$(git diff --shortstat origin/$BASE_BRANCH..HEAD -- specs/$client | wc -l)
+        if [[ $BASE_CHANGED || $changed > 0 ]]; then
+            specs=$(echo $specs | jq --arg client $client '.client |= .+ [$client]')
+        fi
+    fi
+done
+
+# Convert the array to json for the matrix
+if [[ $(echo $specs | jq '.client | length') == 0 ]]; then
+    # client cannot be empty or the matrix will fail
+    matrix='{"client":["no-run"]}'
+else
+    matrix=$(echo $specs | jq -c)
+fi
+
+echo $matrix
