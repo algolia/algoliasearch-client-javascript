e3b7ed8f47fa96c6558270e223256c468f05f97d
Eunjae Lee <eunjae.lee@algolia.com>
2022-02-01 11:14:37 +0100

    fix(js): add client tests (#110)

--- tests/output/javascript/tests/client/search.test.ts
+++ tests/output/javascript/tests/client/search.test.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 // @ts-nocheck
 import { SearchApi, EchoRequester } from '@algolia/client-search';
 
@@ -10,14 +8,14 @@ function createClient(): SearchApi {
   return new SearchApi(appId, apiKey, { requester: new EchoRequester() });
 }
 
-describe('basic', () => {
-  test('client throws with invalid parameters', async () => {
+describe('parameters', () => {
+  test('constructor throws with invalid parameters', async () => {
     let actual;
     await expect(
       new Promise((resolve, reject) => {
         const $client = new SearchApi(
           '',
-          'blah',
+          '',
 
           {
             requester: new EchoRequester(),
@@ -32,5 +30,129 @@ describe('basic', () => {
         }
       })
     ).rejects.toThrow('`appId` is missing.');
+
+    await expect(
+      new Promise((resolve, reject) => {
+        const $client = new SearchApi(
+          '',
+          'my-api-key',
+
+          {
+            requester: new EchoRequester(),
+          }
+        );
+        actual = $client;
+
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow('`appId` is missing.');
+
+    await expect(
+      new Promise((resolve, reject) => {
+        const $client = new SearchApi(
+          'my-app-id',
+          '',
+
+          {
+            requester: new EchoRequester(),
+          }
+        );
+        actual = $client;
+
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow('`apiKey` is missing.');
+  });
+
+  test('`addApiKey` throws with invalid parameters', async () => {
+    const $client = createClient();
+
+    let actual;
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.addApiKey();
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `apiKey` is required when calling `addApiKey`.'
+    );
+
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.addApiKey({});
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `apiKey.acl` is required when calling `addApiKey`.'
+    );
+  });
+
+  test('`addOrUpdateObject` throws with invalid parameters', async () => {
+    const $client = createClient();
+
+    let actual;
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.addOrUpdateObject({
+          objectID: 'my-object-id',
+          body: {},
+        });
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `indexName` is required when calling `addOrUpdateObject`.'
+    );
+
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.addOrUpdateObject({
+          indexName: 'my-index-name',
+          body: {},
+        });
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `objectID` is required when calling `addOrUpdateObject`.'
+    );
+
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.addOrUpdateObject({
+          indexName: 'my-index-name',
+          objectID: 'my-object-id',
+        });
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `body` is required when calling `addOrUpdateObject`.'
+    );
   });
 });
