aad3fdee214e3eacc972f870cc8389c2f360a763
Clément Vannicatte <vannicattec@gmail.com>
2022-03-14 12:15:16 +0100

    fix(recommend): declare type before usage

--- packages/recommend/src/types/RecommendModel.ts
+++ packages/recommend/src/types/RecommendModel.ts
@@ -1,2 +1,2 @@
-export type RecommendModel = 'related-products' | 'bought-together' | TrendingModel;
 export type TrendingModel = 'trending-items' | 'trending-facets';
+export type RecommendModel = 'related-products' | 'bought-together' | TrendingModel;
