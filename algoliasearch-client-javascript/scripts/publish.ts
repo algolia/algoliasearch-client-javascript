853c388a4ea0affcd1c2032711f4b9931a911cf9
Eunjae Lee <karis612@gmail.com>
2022-03-21 15:05:52 +0100

    chore(ci): add release process for js repository (#251)
    * chore(ci): add release process for js repository
    
    * chore: rename
    
    * chore: update github action
    
    * chore: restore test version
    
    * chore: pin devDep
    
    * chore: clean up code
    
    * chore: convert to typescript
    
    * chore: remove version from umbrella package.json for js packages
    
    * Update clients/algoliasearch-client-javascript/scripts/publish.ts
    
    Co-authored-by: Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
    
    * chore: include .npmrc
    
    Co-authored-by: Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>

---
+++ algoliasearch-client-javascript/scripts/publish.ts
@@ -0,0 +1,36 @@
+import fsp from 'fs/promises';
+import path from 'path';
+
+import { execaCommand } from 'execa';
+import semver from 'semver';
+
+async function publish(): Promise<void> {
+  // Read the local version of `algoliasearch/package.json`
+  const { version } = JSON.parse(
+    (
+      await fsp.readFile(
+        path.resolve(
+          __dirname,
+          '..',
+          'packages',
+          'algoliasearch',
+          'package.json'
+        )
+      )
+    ).toString()
+  );
+
+  // Get tag like `alpha`, `beta`, ...
+  const tag = semver.prerelease(version)?.[0];
+
+  await execaCommand(
+    `lerna exec --no-bail npm publish --access public ${
+      tag ? `--tag ${tag}` : ''
+    }`,
+    {
+      shell: 'bash',
+    }
+  );
+}
+
+publish();
