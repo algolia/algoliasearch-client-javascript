9845f04dcae49dbd6b6e644ac548ff3f1cade8bb
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2022-04-05 12:48:19 +0200

    fix(spec): fix wrong query parameters (#330)
    Co-authored-by: Pierre Millot <pierre.millot@algolia.com>

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.languages.TypeScriptNodeClientCodegen;
-
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.servers.Server;
 
@@ -17,5 +18,13 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
   @Override
   public CodegenOperation fromOperation(String path, String httpMethod, Operation operation, List<Server> servers) {
     return Utils.specifyCustomRequest(super.fromOperation(path, httpMethod, operation, servers));
+  }
+
+  @Override
+  protected String getParameterDataType(Parameter parameter, Schema p) {
+    String type = super.getParameterDataType(parameter, p);
+    // openapi generator is wrong, 'object' is not a fit all object, we need 'any'
+    // we use replace because there might be more to this type, like '| undefined'
+    return type.replace("{ [key: string]: object; }", "Record<string, any>");
   }
 }
