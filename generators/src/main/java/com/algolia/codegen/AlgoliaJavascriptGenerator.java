e54df74d0e5ebad5502640628c5874075f5b24d4
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2022-04-06 10:55:02 +0200

    fix(javascript): handle parent in models (#339)

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -1,23 +1,30 @@
 package com.algolia.codegen;
 
-import java.util.List;
-
-import org.openapitools.codegen.CodegenOperation;
-import org.openapitools.codegen.languages.TypeScriptNodeClientCodegen;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.languages.TypeScriptNodeClientCodegen;
 
 public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
+
   @Override
   public String getName() {
     return "algolia-javascript";
   }
 
   @Override
-  public CodegenOperation fromOperation(String path, String httpMethod, Operation operation, List<Server> servers) {
-    return Utils.specifyCustomRequest(super.fromOperation(path, httpMethod, operation, servers));
+  public CodegenOperation fromOperation(
+    String path,
+    String httpMethod,
+    Operation operation,
+    List<Server> servers
+  ) {
+    return Utils.specifyCustomRequest(
+      super.fromOperation(path, httpMethod, operation, servers)
+    );
   }
 
   @Override
@@ -26,5 +33,14 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
     // openapi generator is wrong, 'object' is not a fit all object, we need 'any'
     // we use replace because there might be more to this type, like '| undefined'
     return type.replace("{ [key: string]: object; }", "Record<string, any>");
+  }
+
+  @Override
+  public String toInstantiationType(Schema schema) {
+    String type = super.toInstantiationType(schema);
+
+    // Same as the `getParameterDataType` but for the models, the generator
+    // consider `object` as a fit all which is wrong in TypeScript
+    return type.replace("null<String, object>", "Record<string, any>");
   }
 }
