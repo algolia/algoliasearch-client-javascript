e48fb413870805795685c68e8c8befd6ad76788b
Pierre Millot <pierre.millot@algolia.com>
2022-04-08 11:47:12 +0200

    chore: upgrade typescript version to fix netlify (#355)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  extends: [
-    'algolia',
-    'algolia/typescript',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:yml/standard',
-  ],
+  ignorePatterns: ['.eslintrc.js', '**/node_modules', '**/build', '**/dist', '**/target', '**/.yarn'],
 
-  // yml linter
   overrides: [
     {
+      // yml linter
       files: ['*.yml'],
+
+      extends: [
+        'plugin:yml/standard',
+      ],
+
       parser: 'yaml-eslint-parser',
       plugins: ["automation-custom"],
       rules: {
@@ -41,56 +39,66 @@ module.exports = {
       }
       ]
     },
-  ],
+    {
+      // es linter
+      files: ['*.ts', '*.js'],
 
-  env: {
-    es6: true,
-  },
+      extends: [
+        'algolia',
+        'algolia/typescript',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+      ],
 
-  parser: '@typescript-eslint/parser',
+      env: {
+        es6: true,
+      },
 
-  ignorePatterns: ['.eslintrc.js'],
+      parser: '@typescript-eslint/parser',
 
-  settings: {
-    'import/extensions': ['.js', '.ts'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts'],
-    },
-    'import/ignore': ['node_modules'],
-  },
+      settings: {
+        'import/extensions': ['.js', '.ts'],
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts'],
+        },
+        'import/ignore': ['node_modules'],
+      },
 
-  plugins: ['algolia', 'unused-imports'],
+      plugins: ['algolia', 'unused-imports'],
 
-  rules: {
-    // disabled
-    'no-bitwise': 0,
-    '@typescript-eslint/no-namespace': 0,
-    'max-classes-per-file': 0,
-    'no-continue': 0,
-    '@typescript-eslint/prefer-enum-initializers': 0,
+      rules: {
+        // disabled
+        'no-bitwise': 0,
+        '@typescript-eslint/no-namespace': 0,
+        'max-classes-per-file': 0,
+        'no-continue': 0,
+        '@typescript-eslint/prefer-enum-initializers': 0,
 
-    '@typescript-eslint/no-unused-vars': 2,
-    'unused-imports/no-unused-imports-ts': 2,
-    '@typescript-eslint/member-ordering': [
-      'error',
-      {
-        default: [
-          'protected-instance-method',
-          'private-instance-method',
-          'public-instance-method',
+        '@typescript-eslint/no-unused-vars': 2,
+        'unused-imports/no-unused-imports-ts': 2,
+        '@typescript-eslint/member-ordering': [
+          'error',
+          {
+            default: [
+              'protected-instance-method',
+              'private-instance-method',
+              'public-instance-method',
+            ],
+          },
+        ],
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              object: {
+                message: 'Use Record instead',
+                fixWith: 'Record<string, any>',
+              },
+            },
+          },
         ],
       },
-    ],
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          object: {
-            message: 'Use Record instead',
-            fixWith: 'Record<string, any>',
-          },
-        },
-      },
-    ],
-  },
+    }
+  ],
 };
