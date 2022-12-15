5700dea65888dd71614566ac3219e9314eecd637
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2022-12-15 16:32:25 +0100

    fix: remove unused props for trending facets

--- packages/recommend/src/types/TrendingFacetsQuery.ts
+++ packages/recommend/src/types/TrendingFacetsQuery.ts
@@ -1,3 +1,3 @@
 import { TrendingQuery } from './TrendingQuery';
 
-export type TrendingFacetsQuery = Omit<TrendingQuery, 'model' | 'facetValue'>;
+export type TrendingFacetsQuery = Omit<TrendingQuery, 'model' | 'facetValue' | 'fallbackParameters' | 'queryParameters'>;
