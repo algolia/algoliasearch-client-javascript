9de45790217bece14946b1dc89a564a0b26d8aba
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2024-12-14 14:37:44 +0100

    feat: removed node apis from worker build

--- packages/algoliasearch/__tests__/algoliasearch.fetch.test.ts
+++ packages/algoliasearch/__tests__/algoliasearch.fetch.test.ts
@@ -1,38 +1,38 @@
-import { expect, test, vi } from 'vitest';
+import { expect, test, vi } from "vitest";
 
-import { LogLevelEnum } from '../../client-common/src/types';
-import { createConsoleLogger } from '../../logger-console/src/logger';
-import { algoliasearch, apiClientVersion } from '../builds/fetch';
+import { LogLevelEnum } from "../../client-common/src/types";
+import { createConsoleLogger } from "../../logger-console/src/logger";
+import { algoliasearch, apiClientVersion } from "../builds/fetch";
 
-test('sets the ua', () => {
-  const client = algoliasearch('APP_ID', 'API_KEY');
+test("sets the ua", () => {
+  const client = algoliasearch("APP_ID", "API_KEY");
   expect(client.transporter.algoliaAgent).toEqual({
     add: expect.any(Function),
     value: expect.stringContaining(`Algolia for JavaScript (${apiClientVersion}); Search (${apiClientVersion}); Fetch`),
   });
 });
 
-test('forwards node search helpers', () => {
-  const client = algoliasearch('APP_ID', 'API_KEY');
+test("forwards node search helpers", () => {
+  const client = algoliasearch("APP_ID", "API_KEY");
   expect(client.generateSecuredApiKey).not.toBeUndefined();
   expect(client.getSecuredApiKeyRemainingValidity).not.toBeUndefined();
-  expect(() => {
-    const resp = client.generateSecuredApiKey({ parentApiKey: 'foo', restrictions: { validUntil: 200 } });
+  expect(async () => {
+    const resp = await client.generateSecuredApiKey({ parentApiKey: "foo", restrictions: { validUntil: 200 } });
     client.getSecuredApiKeyRemainingValidity({ securedApiKey: resp });
   }).not.toThrow();
 });
 
-test('with logger', () => {
-  vi.spyOn(console, 'debug');
-  vi.spyOn(console, 'info');
-  vi.spyOn(console, 'error');
+test("with logger", () => {
+  vi.spyOn(console, "debug");
+  vi.spyOn(console, "info");
+  vi.spyOn(console, "error");
 
-  const client = algoliasearch('APP_ID', 'API_KEY', {
+  const client = algoliasearch("APP_ID", "API_KEY", {
     logger: createConsoleLogger(LogLevelEnum.Debug),
   });
 
   expect(async () => {
-    await client.setSettings({ indexName: 'foo', indexSettings: {} });
+    await client.setSettings({ indexName: "foo", indexSettings: {} });
     expect(console.debug).toHaveBeenCalledTimes(1);
     expect(console.info).toHaveBeenCalledTimes(1);
     expect(console.error).toHaveBeenCalledTimes(1);
--- packages/client-search/builds/fetch.ts
+++ packages/client-search/builds/fetch.ts
@@ -2,32 +2,51 @@
 
 export type SearchClient = ReturnType<typeof createSearchClient> & SearchClientNodeHelpers;
 
-import { createMemoryCache, createNullCache, createNullLogger, serializeQueryParameters } from '@algolia/client-common';
-import { createFetchRequester } from '@algolia/requester-fetch';
+import { createMemoryCache, createNullCache, createNullLogger, serializeQueryParameters } from "@algolia/client-common";
+import { createFetchRequester } from "@algolia/requester-fetch";
 
-import type { ClientOptions } from '@algolia/client-common';
+import type { ClientOptions } from "@algolia/client-common";
 
-import { createSearchClient } from '../src/searchClient';
+import { createSearchClient } from "../src/searchClient";
 
-export { apiClientVersion } from '../src/searchClient';
+export { apiClientVersion } from "../src/searchClient";
 
-export * from '../model';
+export * from "../model";
 
 import type {
   GenerateSecuredApiKeyOptions,
   GetSecuredApiKeyRemainingValidityOptions,
   SearchClientNodeHelpers,
-} from '../model';
+} from "../model";
 
-import { createHmac } from 'node:crypto';
+async function getCryptoKey(secret: string): Promise<CryptoKey> {
+  const secretBuf = new TextEncoder().encode(secret);
+  return await crypto.subtle.importKey("raw", secretBuf, { name: "HMAC", hash: "SHA-256" }, false, ["sign"]);
+}
+
+async function generateHmacHex(cryptoKey: CryptoKey, queryParameters: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const queryParametersUint8Array = encoder.encode(queryParameters);
+  const signature = await crypto.subtle.sign("HMAC", cryptoKey, queryParametersUint8Array);
+  return Array.from(new Uint8Array(signature))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+async function generateBase64Hmac(parentApiKey: string, queryParameters: string): Promise<string> {
+  const crypotKey = await getCryptoKey(parentApiKey);
+  const hmacHex = await generateHmacHex(crypotKey, queryParameters);
+  const combined = hmacHex + queryParameters;
+  return btoa(combined);
+}
 
 export function searchClient(appId: string, apiKey: string, options?: ClientOptions): SearchClient {
-  if (!appId || typeof appId !== 'string') {
-    throw new Error('`appId` is missing.');
+  if (!appId || typeof appId !== "string") {
+    throw new Error("`appId` is missing.");
   }
 
-  if (!apiKey || typeof apiKey !== 'string') {
-    throw new Error('`apiKey` is missing.');
+  if (!apiKey || typeof apiKey !== "string") {
+    throw new Error("`apiKey` is missing.");
   }
 
   return {
@@ -40,7 +59,7 @@ export function searchClient(appId: string, apiKey: string, options?: ClientOpti
         write: 30000,
       },
       logger: createNullLogger(),
-      algoliaAgents: [{ segment: 'Fetch' }],
+      algoliaAgents: [{ segment: "Fetch" }],
       requester: createFetchRequester(),
       responsesCache: createNullCache(),
       requestsCache: createNullCache(),
@@ -55,7 +74,10 @@ export function searchClient(appId: string, apiKey: string, options?: ClientOpti
      * @param generateSecuredApiKey.parentApiKey - The base API key from which to generate the new secured one.
      * @param generateSecuredApiKey.restrictions - A set of properties defining the restrictions of the secured API key.
      */
-    generateSecuredApiKey: ({ parentApiKey, restrictions = {} }: GenerateSecuredApiKeyOptions): string => {
+    generateSecuredApiKey: async ({
+      parentApiKey,
+      restrictions = {},
+    }: GenerateSecuredApiKeyOptions): Promise<string> => {
       let mergedRestrictions = restrictions;
       if (restrictions.searchParams) {
         // merge searchParams with the root restrictions
@@ -69,18 +91,13 @@ export function searchClient(appId: string, apiKey: string, options?: ClientOpti
 
       mergedRestrictions = Object.keys(mergedRestrictions)
         .sort()
-        .reduce(
-          (acc, key) => {
-            acc[key] = (mergedRestrictions as any)[key];
-            return acc;
-          },
-          {} as Record<string, unknown>,
-        );
+        .reduce((acc, key) => {
+          acc[key] = (mergedRestrictions as any)[key];
+          return acc;
+        }, {} as Record<string, unknown>);
 
       const queryParameters = serializeQueryParameters(mergedRestrictions);
-      return Buffer.from(
-        createHmac('sha256', parentApiKey).update(queryParameters).digest('hex') + queryParameters,
-      ).toString('base64');
+      return generateBase64Hmac(parentApiKey, queryParameters);
     },
 
     /**
@@ -91,12 +108,12 @@ export function searchClient(appId: string, apiKey: string, options?: ClientOpti
      * @param getSecuredApiKeyRemainingValidity.securedApiKey - The secured API key generated with the `generateSecuredApiKey` method.
      */
     getSecuredApiKeyRemainingValidity: ({ securedApiKey }: GetSecuredApiKeyRemainingValidityOptions): number => {
-      const decodedString = Buffer.from(securedApiKey, 'base64').toString('ascii');
+      const decodedString = atob(securedApiKey);
       const regex = /validUntil=(\d+)/;
       const match = decodedString.match(regex);
 
       if (match === null) {
-        throw new Error('validUntil not found in given secured api key.');
+        throw new Error("validUntil not found in given secured api key.");
       }
 
       return parseInt(match[1], 10) - Math.round(new Date().getTime() / 1000);
