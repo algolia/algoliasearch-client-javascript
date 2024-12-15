b69eec8fdb84b4a3a778709bce4b1b05fb7452be
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2024-12-15 14:51:30 +0100

    feat: formatting

--- packages/algoliasearch/__tests__/algoliasearch.node.test.ts
+++ packages/algoliasearch/__tests__/algoliasearch.node.test.ts
@@ -9,8 +9,8 @@ test('sets the ua', () => {
   expect(client.transporter.algoliaAgent).toEqual({
     add: expect.any(Function),
     value: expect.stringContaining(
-      `Algolia for JavaScript (${apiClientVersion}); Search (${apiClientVersion}); Node.js`
-    )
+      `Algolia for JavaScript (${apiClientVersion}); Search (${apiClientVersion}); Node.js`,
+    ),
   });
 });
 
